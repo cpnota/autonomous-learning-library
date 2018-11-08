1c5ac3d04752918eaedc93c6f64f632ccbab2bf9
Chris Nota <cpnota@gmail.com>
2018-11-08 17:29:27 -0500

    fix parameter order (#6)

--- all/approximation/traces/accumulating_test.py
+++ all/approximation/traces/accumulating_test.py
@@ -7,11 +7,11 @@ import numpy as np
 import unittest
 
 space = Box(low=0, high=1, shape=(2,))
+state = np.array([0.5, 1])
 
 class Env:
   def __init__(self):
     self.done = False
-x = np.array([0.5, 1])
 
 class TestAccumulatingTraces(unittest.TestCase):
   def setUp(self):
@@ -21,37 +21,38 @@ class TestAccumulatingTraces(unittest.TestCase):
     self.traces = AccumulatingTraces(self.approximation, self.env, 0.5)
 
   def test_init(self):
-    np.testing.assert_equal(self.traces.call(x), np.array([0, 0, 0]))
+    np.testing.assert_equal(self.traces.call(state), np.array([0, 0, 0]))
 
   def test_update_once(self):
-    np.testing.assert_equal(self.traces.call(x), np.array([0, 0, 0]))
-    self.traces.update(x, 1, 1)
-    np.testing.assert_allclose(self.traces.call(x), np.array([0, 0.6, 0]))
+    np.testing.assert_equal(self.traces.call(state), np.array([0, 0, 0]))
+    self.traces.update(1, state, 1)
+    np.testing.assert_allclose(self.traces.call(state), np.array([0, 0.6, 0]))
 
   def test_call_one(self):
-    np.testing.assert_equal(self.traces.call(x), np.array([0, 0, 0]))
-    self.traces.update(x, 1, 1)
-    np.testing.assert_approx_equal(self.traces.call(x, 1), 0.6)
+    np.testing.assert_equal(self.traces.call(state), np.array([0, 0, 0]))
+    self.traces.update(1, state, 1)
+    np.testing.assert_approx_equal(self.traces.call(state, 1), 0.6)
 
   def test_update_multiple(self):
-    np.testing.assert_equal(self.traces.call(x), np.array([0, 0, 0]))
-    self.traces.update(x, 1, 1)
-    self.traces.update(x, 1, 1)
+    np.testing.assert_equal(self.traces.call(state), np.array([0, 0, 0]))
+    self.traces.update(1, state, 1)
+    self.traces.update(1, state, 1)
     # should be 0.6 + (0.6 + 0.3)
-    np.testing.assert_allclose(self.traces.call(x), np.array([0, 1.5, 0]))
-    self.traces.update(x, 2, 1)
-    np.testing.assert_allclose(self.traces.call(x), np.array([0, 1.95, 0.6]))
+    np.testing.assert_allclose(self.traces.call(state), np.array([0, 1.5, 0]))
+    self.traces.update(1, state, 2)
+    np.testing.assert_allclose(self.traces.call(state), np.array([0, 1.95, 0.6]))
 
   def test_clears_traces_on_done(self):
-    np.testing.assert_equal(self.traces.call(x), np.array([0, 0, 0]))
-    self.traces.update(x, 1, 1)
+    np.testing.assert_equal(self.traces.call(state), np.array([0, 0, 0]))
+    self.traces.update(1, state, 1)
     self.env.done = True
-    self.traces.update(x, 1, 1)
+    self.traces.update(1, state, 1)
     # should be 0.6 + (0.6 + 0.3)
-    np.testing.assert_allclose(self.traces.call(x), np.array([0, 1.5, 0]))
-    self.traces.update(x, 2, 1)
+    np.testing.assert_allclose(self.traces.call(state), np.array([0, 1.5, 0]))
+    self.traces.update(1, state, 2)
     # these should be cleared compared to above
-    np.testing.assert_allclose(self.traces.call(x), np.array([0, 1.5, 0.6]))
+    np.testing.assert_allclose(self.traces.call(state), np.array([0, 1.5, 0.6]))
 
 if __name__ == '__main__':
     unittest.main()
+
