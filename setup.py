6260ded5a990066670d382eb04c9edfccd569ad1
Scott <scott.m.jordan91@gmail.com>
2018-11-09 13:21:23 -0500

    added an initial setup.py file for the repo

---
+++ setup.py
@@ -0,0 +1,18 @@
+# from sys import platform as _platform
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="all",
+    version="0.0.1",
+    description=("A reinforcement learning library in python"),
+    packages=find_packages(),
+    url="https://github.com/cpnota/autonomous-learning-library.git",
+    install_requires=[
+        "numpy",
+        "gym",
+        "pylint",
+    ],
+)
+
