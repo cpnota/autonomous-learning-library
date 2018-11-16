0d4ee3cc45820b5d17510c3f94cead318197cc95
Chris Nota <cpnota@gmail.com>
2018-11-16 11:22:12 -0500

    add actor_critic_demo

--- all/presets/fourier/actor_critic.py
+++ all/presets/fourier/actor_critic.py
@@ -5,7 +5,7 @@ from all.policies import SoftmaxLinear
 from all.agents import ActorCritic
 
 
-def actor_critic(env, alpha=0.05, order=1):
+def actor_critic(env, alpha=0.001, order=1):
     num_actions = env.env.action_space.n
     basis = FourierBasis(env.env.observation_space, order)
     v = LinearStateValue(alpha, basis)
---
+++ all/presets/fourier/actor_critic_demo.py
@@ -0,0 +1,31 @@
+from all.environments import GymWrapper
+from all.presets.fourier import actor_critic
+
+
+def run_episode(agent, env):
+    env.reset()
+    agent.new_episode(env)
+    returns = 0
+
+    while not env.done:
+        env.env.render()
+        agent.act()
+        returns += env.reward
+
+    print('Returns: ', returns)
+
+
+def run():
+    env = GymWrapper('MountainCar-v0')
+    env.env._max_episode_steps = 3000  # defaults to 200
+    agent = actor_critic(env, alpha=0.02)
+
+    for _ in range(1):
+        for _ in range(200):
+            run_episode(agent, env)
+
+    env.close()
+
+
+if __name__ == '__main__':
+    run()
