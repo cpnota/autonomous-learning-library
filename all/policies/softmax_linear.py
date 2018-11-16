627215236031831a49105d75fdb5f25f9197a9d5
Chris Nota <cpnota@gmail.com>
2018-11-16 11:03:10 -0500

    add softmax_linear policy

---
+++ all/policies/softmax_linear.py
@@ -0,0 +1,37 @@
+import numpy as np
+from all.policies.policy import Policy
+
+class SoftmaxLinear(Policy):
+    def __init__(self, learning_rate, basis, actions):
+        self.learning_rate = learning_rate
+        self.basis = basis
+        self.weights = np.zeros((actions, self.basis.num_features))
+
+    def call(self, state, action=None, prob=False):
+        features = self.basis.features(state)
+        probabilities = self.probabilities(features)
+        return np.random.choice(probabilities.shape[0], p=probabilities)
+
+    def update(self, error, state, action):
+        self.weights += self.learning_rate * error * self.gradient(state, action)
+
+    def gradient(self, state, action):
+        features = self.basis.features(state)
+        neg_probabilities = -self.probabilities(features)
+        neg_probabilities[action] += 1
+        return np.outer(neg_probabilities, features)
+
+    def apply(self, gradient):
+        self.weights += self.learning_rate * gradient
+
+    @property
+    def parameters(self):
+        return self.weights
+
+    @parameters.setter
+    def parameters(self, parameters):
+        self.weights = parameters
+
+    def probabilities(self, features):
+        action_scores = np.exp(self.weights.dot(features))
+        return action_scores / np.sum(action_scores)
---
+++ all/policies/softmax_linear_test.py
@@ -0,0 +1,67 @@
+import unittest
+from gym.spaces import Box
+import numpy as np
+from all.policies import SoftmaxLinear
+from all.approximation.bases.fourier import FourierBasis
+
+NUM_ACTIONS = 3
+LEARNING_RATE = 0.1
+SPACE = Box(low=0, high=1, shape=(2,), dtype=np.float32)
+BASIS = FourierBasis(SPACE, 2)
+STATE = np.array([0.5, 1])
+FEATURES = BASIS.features(STATE)
+
+
+class TestLinearStateDiscreteActionValue(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(0)
+        self.policy = SoftmaxLinear(
+            LEARNING_RATE, BASIS, actions=NUM_ACTIONS)
+
+    def test_call_initial(self):
+        np.testing.assert_equal(
+            self.policy.call(STATE), 1)
+
+    def test_update(self):
+        self.policy.update(1, STATE, 0)
+        np.testing.assert_allclose(
+            self.policy.probabilities(FEATURES), 
+            np.array([0.47673, 0.261635, 0.261635]
+        ))
+
+    def test_gradient(self):
+        np.testing.assert_allclose(
+            self.policy.gradient(STATE, 1), 
+            np.array([
+                -FEATURES / 3,
+                FEATURES * 2/3,
+                -FEATURES / 3
+            ])
+        )
+
+    def test_get_parameters(self):
+        np.testing.assert_equal(
+            self.policy.parameters,
+            np.zeros((NUM_ACTIONS, BASIS.num_features))
+        )
+
+    def test_set_parameters(self):
+        new_parameters = np.ones((NUM_ACTIONS, BASIS.num_features))
+        self.policy.parameters = new_parameters
+        np.testing.assert_equal(
+            self.policy.parameters,
+            new_parameters
+        )
+
+    def test_update_parameters(self):
+        gradient = np.ones((NUM_ACTIONS, BASIS
+                            .num_features))
+        self.policy.apply(gradient)
+        np.testing.assert_equal(
+            self.policy.parameters,
+            LEARNING_RATE * gradient
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
