e6db8cc6ba458d5127b1c1c4bb1f2fae73e2e769
Chris Nota <cpnota@gmail.com>
2019-01-03 17:04:14 -0500

    create linear model folder

--- all/presets/linear/sarsa.py
+++ all/presets/linear/sarsa.py
@@ -1,13 +1,11 @@
 # /Users/cpnota/repos/autonomous-learning-library/all/approximation/value/action/torch.py
-from torch import nn
 from all.approximation.action_value import TabularActionValue
 from all.agents import Sarsa
 from all.policies.greedy import Greedy
+from .models import tabular_action
 
 def sarsa(env):
-    model = nn.Sequential(
-        nn.Linear(env.state_space.shape[0], env.action_space.n)
-    )
+    model = tabular_action(env)
     q = TabularActionValue(model)
     policy = Greedy(q, epsilon=0.1)
     return Sarsa(q, policy)
