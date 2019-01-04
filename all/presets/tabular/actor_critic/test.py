c0fe144b774dcec0ff79007fb3214b23833e9d5d
Chris Nota <cpnota@gmail.com>
2019-01-04 11:38:20 -0500

    add tabular actor_critic

---
+++ all/presets/tabular/actor_critic/test.py
@@ -0,0 +1,22 @@
+import unittest
+from all.environments import GymWrapper
+from all.presets.tabular import actor_critic
+
+
+class TestSarsa(unittest.TestCase):
+    def test_runs(self):
+        env = GymWrapper('FrozenLake-v0')
+        agent = actor_critic(env)
+
+        env.reset()
+        agent.new_episode(env)
+
+        agent.act()
+        agent.act()
+        agent.act()
+
+        self.assertIsNotNone(env.state)
+
+
+if __name__ == '__main__':
+    unittest.main()
