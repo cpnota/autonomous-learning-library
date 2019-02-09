5da444d6126a6cf299e1d66a50990638e0bdb970
Chris Nota <cpnota@gmail.com>
2019-02-09 17:55:58 -0500

    clean up value function

--- all/approximation/v_network.py
+++ all/approximation/v_network.py
@@ -26,15 +26,19 @@ class ValueNetwork(ValueFunction):
 
     def _eval(self, states):
         if isinstance(states, list):
-            non_terminal_states = [
-                state for state in states if state is not None]
-            non_terminal_indexes = [
-                i for i, state in enumerate(states) if state is not None]
-            values = self.model(
-                torch.cat(non_terminal_states).float())
-            result = torch.zeros((len(states), values.shape[1]))
-            result[non_terminal_indexes] = values
-            return result.squeeze(1)
+            return self._eval_list(states)
         if states is None:
             return torch.zeros(1)
         return self.model(states.float()).squeeze(1)
+
+    def _eval_list(self, states):
+        (non_terminal_states, non_terminal_indexes) = get_non_terminal_states(states)
+        non_terminal_values = self.model(torch.cat(non_terminal_states).float())
+        result = torch.zeros((len(states), non_terminal_values.shape[1]))
+        result[non_terminal_indexes] = non_terminal_values
+        return result.squeeze(1)
+
+def get_non_terminal_states(states):
+    non_terminal_states = [state for state in states if state is not None]
+    non_terminal_indexes = [i for i, state in enumerate(states) if state is not None]
+    return (non_terminal_states, non_terminal_indexes)
