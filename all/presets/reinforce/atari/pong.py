816a1fae6a4d5acca64adb04147f025255206917
Chris Nota <cpnota@gmail.com>
2019-02-10 20:48:38 -0500

    change reinforce initial parameters

--- all/presets/reinforce/atari/pong.py
+++ all/presets/reinforce/atari/pong.py
@@ -6,10 +6,10 @@ from all.presets.reinforce import reinforce_atari
 def run():
     # env = AtariEnvironment('Pong')
     env = PongEnvironment()
-    experiment = Experiment(env, episodes=20000, trials=1)
+    experiment = Experiment(env, episodes=100000, trials=1)
     experiment.run(
         reinforce_atari(),
-        plot_every=100,
+        plot_every=1000,
         print_every=1,
         render=True
     )
