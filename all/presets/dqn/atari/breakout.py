56e3e62fdba5cb8142a44b849fa9a68266ee5556
Chris Nota <cpnota@gmail.com>
2019-02-18 15:49:36 -0500

    longer experiment

--- all/presets/dqn/atari/breakout.py
+++ all/presets/dqn/atari/breakout.py
@@ -4,7 +4,7 @@ from all.presets.dqn import dqn
 
 def run():
     env = AtariEnvironment('Breakout')
-    experiment = Experiment(env, episodes=20000, trials=1)
+    experiment = Experiment(env, episodes=40000, trials=1)
     experiment.run(
         dqn(),
         plot_every=100,
