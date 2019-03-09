5f44cfbd29945e310a04f9bf30a7154cb7bce3f7
Chris Nota <cpnota@gmail.com>
2019-03-09 16:23:40 -0500

    add DeepmindAtariBody

---
+++ all/bodies/deepmind_atari.py
@@ -0,0 +1,55 @@
+import torch
+from .abstract import Body
+
+class DeepMindAtariBody(Body):
+    def __init__(
+            self,
+            agent,
+            env,
+            frameskip=4
+    ):
+        self.agent = agent
+        self.env = env
+        self.frameskip = frameskip
+        self._state = None
+        self._action = None
+        self._skipped_frames = 0
+        self._reward = 0
+
+    def initial(self, state, info=None):
+        self._skipped_frames = 1
+        self._state = [preprocess(state)] * self.frameskip
+        self._action = self.agent.initial(self._stack_state(), info)
+        self._skipped_frames = 1
+        return self._action
+
+    def act(self, state, reward, info=None):
+        self._state.append(preprocess(state))
+        self._reward += reward
+        self._skipped_frames += 1
+        if self._skipped_frames == self.frameskip:
+            self._action = self.agent.act(
+                self._stack_state(),
+                self._reward,
+                info
+            )
+            self._state = []
+            self._reward = 0
+            self._skipped_frames = 0
+        return self._action
+
+    def terminal(self, reward, info=None):
+        self._reward += reward
+        return self.agent.terminal(self._reward, info)
+
+    def _stack_state(self):
+        return torch.cat(self._state).unsqueeze(0)
+
+def to_grayscale(frame):
+    return torch.mean(frame.float(), dim=3).byte()
+
+def downsample(frame):
+    return frame[:, ::2, ::2]
+
+def preprocess(frame):
+    return to_grayscale(downsample(frame))
---
+++ all/bodies/deepmind_atari_test.py
@@ -0,0 +1,71 @@
+import unittest
+import torch
+import torch_testing as tt
+from all.agents import Agent
+from all.environments import GymEnvironment
+from all.bodies.deepmind_atari import DeepMindAtariBody
+
+class MockAgent(Agent):
+    def __init__(self):
+        self.state = None
+        self.reward = None
+        self.info = None
+
+    def initial(self, state, info=None):
+        self.state = state
+        self.info = info
+        return torch.tensor([0])
+
+    def act(self, state, reward, info=None):
+        self.state = state
+        self.reward = reward
+        self.info = info
+        return torch.tensor([0])
+
+    def terminal(self, reward, info=None):
+        self.reward = reward
+        self.info = info
+
+class DeepMindAtariBodyTest(unittest.TestCase):
+    def setUp(self):
+        self.agent = MockAgent()
+        self.env = GymEnvironment('PongNoFrameskip-v4')
+        self.body = DeepMindAtariBody(self.agent, self.env)
+
+    def test_initial_state(self):
+        self.env.reset()
+        action = self.body.initial(self.env.state)
+        tt.assert_equal(action, torch.tensor([0]))
+        self.assertEqual(self.agent.state.shape, (1, 4, 105, 80))
+
+    def test_second_state(self):
+        self.env.reset()
+        self.env.step(self.body.initial(self.env.state))
+        action = self.body.act(self.env.state, self.env.reward)
+        tt.assert_equal(action, torch.tensor([0]))
+        self.assertEqual(self.agent.state.shape, (1, 4, 105, 80))
+
+    def test_several_steps(self):
+        self.env.reset()
+        self.env.step(self.body.initial(self.env.state))
+        for _ in range(10):
+            action = self.body.act(self.env.state, -1)
+            self.env.step(action)
+        tt.assert_equal(action, torch.tensor([0]))
+        self.assertEqual(self.agent.state.shape, (1, 4, 105, 80))
+        self.assertEqual(self.agent.reward, -4)
+
+    def test_terminal_state(self):
+        self.env.reset()
+        self.env.step(self.body.initial(self.env.state))
+        for _ in range(12):
+            # custom rewards
+            action = self.body.act(self.env.state, -1)
+            self.env.step(action)
+        self.body.terminal(-1)
+        tt.assert_equal(action, torch.tensor([0]))
+        self.assertEqual(self.agent.state.shape, (1, 4, 105, 80))
+        self.assertEqual(self.agent.reward, -2)
+
+if __name__ == '__main__':
+    unittest.main()
