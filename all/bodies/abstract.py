a682030159514cc23432ef9755c8deb50f47d0eb
Chris Nota <cpnota@gmail.com>
2019-03-09 15:06:42 -0500

    add Body interface

---
+++ all/bodies/abstract.py
@@ -0,0 +1,22 @@
+from abc import abstractmethod
+from ..agents import Agent
+
+class Body(Agent):
+    """
+    A Body wraps a reinforcment learning Agent, altering its inputs and ouputs.
+
+    The Body API is identical to the Agent API from the perspective of the
+    rest of the system. This base class is provided only for semantic clarity.
+    """
+
+    @abstractmethod
+    def initial(self, state, info=None):
+        """See Agent"""
+
+    @abstractmethod
+    def act(self, state, reward, info=None):
+        """See Agent"""
+
+    @abstractmethod
+    def terminal(self, reward, info=None):
+        """See Agent"""
