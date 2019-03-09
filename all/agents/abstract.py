4b86d817accdde18790a72bdd04f5efaee1e060e
Chris Nota <cpnota@gmail.com>
2019-03-09 09:29:30 -0500

    convert Agent to functional API

--- all/agents/abstract.py
+++ all/agents/abstract.py
@@ -12,38 +12,65 @@ class Agent(ABC):
     """
 
     @abstractmethod
-    def new_episode(self, env):
+    def initial(self, state, info={}):
         """
-        Prepare the Agent for a new episode.
+        Choose an action in the initial state of a new episode.
 
         Reinforcement learning problems are often broken down into sequences called "episodes".
         An episode is a self-contained sequence of states, actions, and rewards.
         A "trial" consists of multiple episodes, and represents the lifetime of an agent.
-        In between episodes, the agent usually needs to reset some internal state.
-        The method allows the agent to receive the specific environment instance
-        for the upcoming epsiodes, and reset any internal state.
+        This method is called at the beginning of an episode.
 
         Parameters
         ----------
-        env : Environment
-            Environment object for the next episode.
+        state: The initial state of the new episode
+        info (optional): The info object from the environment
+
+        Returns
+        _______
+        action: The action to take in the initial state
         """
 
     @abstractmethod
-    def act(self):
+    def act(self, state, reward, info={})
         """
-        Choose an action, apply it to the environment, and update self.
+        Select an action for the current timestep and update internal parameters.
 
         In general, a reinforcement learning agent does several things during a timestep:
         1. Choose an action,
-        2. Dispatch it to the environment (i.e. env.step(action))
-        3. Compute the TD error
-        4. Update the value function and/or policy
+        2. Compute the TD error from the previous time step
+        3. Update the value function and/or policy
         The order of these steps differs depending on the agent.
         This method allows the agent to do whatever is necessary for itself on a given timestep.
-        The most important step is to dispatch an action to the environment.
+        However, the agent must ultimately return an action.
 
         Parameters
         ----------
+        state: The environment state at the current timestep
+        reward: The reward from the previous timestep
+        info (optional): The info object from the environment
+
+        Returns
+        _______
+        action: The action to take at the current timestep
+        """
+
+    @abstractmethod
+    def terminal(self, reward, info={})
+        """
+        Accept the final reward of the episode and perform final updates.
+
+        After the final action is selected, it is still necessary to
+        consider the reward given on the final timestep. This method
+        provides a hook where the agent can examine this reward
+        and perform any necessary updates.
+
+        Parameters
+        ----------
+        reward: The reward from the previous timestep
+        info (optional): The info object from the environment
+
+        Returns
+        _______
         None
         """
