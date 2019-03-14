fb03b6a33c57ffe29539a96d8819f96656a11ff6
Chris Nota <cpnota@gmail.com>
2019-03-13 21:50:13 -0400

    add episodic lives

--- all/bodies/deepmind_atari.py
+++ all/bodies/deepmind_atari.py
@@ -36,13 +36,15 @@ class DeepmindAtariBody(Body):
         self._lives = 0
 
     def initial(self, state, info=None):
-        self._state = [self._preprocess_initial(state)] * self.frameskip
-        self._info = info
+        self._set_initial_state(state, info)
         self._choose_action()
         return self._action
 
     def act(self, state, reward, info=None):
         self._update_state(state, reward, info)
+        if self._lost_life():
+            self.terminal(0, info)
+            self.initial(state, info)
         if self._should_choose_action():
             self._choose_action()
         return self._action
@@ -52,11 +54,25 @@ class DeepmindAtariBody(Body):
         self._info = info
         return self.agent.terminal(self._reward, self._info)
 
+    def _set_initial_state(self, state, info):
+        self._state = [self._preprocess_initial(state)] * self.frameskip
+        self._reward = 0
+        self._info = info
+        self._lives = self._get_lives()
+
     def _update_state(self, state, reward, info):
         self._state.append(self._preprocess(state))
         self._reward += clip(reward)
         self._info = info
         self._skipped_frames += 1
+
+    def _lost_life(self):
+        lives = self._get_lives()
+        return lives < self._lives and lives > 0
+
+    def _get_lives(self):
+        # pylint: disable=protected-access
+        return self.env._env.unwrapped.ale.lives()
 
     def _should_choose_action(self):
         return self._skipped_frames == self.frameskip
@@ -79,9 +95,6 @@ class DeepmindAtariBody(Body):
         deflickered_frame = deflicker(frame, self._previous_frame)
         self._previous_frame = frame
         return to_grayscale(downsample(deflickered_frame))
-
-    def _get_lives(self):
-        return self.env._env.unwrapped.ale.lives()
 
 def stack(frames):
     return torch.cat(frames).unsqueeze(0)
