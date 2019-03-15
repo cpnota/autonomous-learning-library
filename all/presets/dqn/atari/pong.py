d1e802e711bd476ffd6399f1924ca245798811d8
Chris Nota <cpnota@gmail.com>
2019-03-15 15:51:03 -0400

    match rainbow pong hyperparameters

--- all/presets/dqn/atari/pong.py
+++ all/presets/dqn/atari/pong.py
@@ -11,13 +11,13 @@ def run():
             # parameters from:
             # https://medium.com/mlreview/speeding-up-dqn-on-pytorch-solving-pong-in-30-minutes-81a1bd2dff55
             replay_buffer_size=100000,
-            replay_start_size=40000,
+            replay_start_size=10000,
             initial_exploration=1.00,
             final_exploration=0.02,
             final_exploration_frame=400000,
             target_update_frequency=1000,
             update_frequency=2,
-            minibatch_size=2 * 32
+            minibatch_size=2 * 32,
         ),
         plot_every=1,
         print_every=1,
