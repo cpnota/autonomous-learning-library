9629dd368d26d866fac6ecc8eb0ce83fe4f6842e
Chris Nota <cpnota@gmail.com>
2019-03-15 11:59:38 -0400

    tweak pong dqn hyperparameters

--- all/presets/dqn/atari/pong.py
+++ all/presets/dqn/atari/pong.py
@@ -11,10 +11,10 @@ def run():
             # parameters from:
             # https://medium.com/mlreview/speeding-up-dqn-on-pytorch-solving-pong-in-30-minutes-81a1bd2dff55
             replay_buffer_size=100000,
-            replay_start_size=10000,
+            replay_start_size=40000,
             initial_exploration=1.00,
             final_exploration=0.02,
-            final_exploration_frame=100000,
+            final_exploration_frame=400000,
             target_update_frequency=1000,
             update_frequency=2,
             minibatch_size=2 * 32
