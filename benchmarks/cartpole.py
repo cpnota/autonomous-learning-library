cb5faba42275f05438f73761e82cd53a0571ed37
Chris Nota <cpnota@gmail.com>
2019-03-22 18:14:06 -0400

    make generic benchmark runner

--- benchmarks/cartpole.py
+++ benchmarks/cartpole.py
@@ -1,31 +1,6 @@
-import subprocess
-import sys
 from all.environments import GymEnvironment
-from all.experiments import Experiment
 from all.presets import classic_control
-
-def run():
-    try:
-        agent_name = sys.argv[1]
-    except IndexError:
-        print('Usage: python benchmarks/cartpole.py [agent_name]')
-        exit(1)
-
-    try:
-        agent = getattr(classic_control, agent_name)
-    except NameError:
-        print('Unknown agent: ' + agent_name)
-        exit(1)
-
-
-    result = subprocess.run(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE)
-    rev = result.stdout.decode('utf-8')
-
-    experiment = Experiment(
-        GymEnvironment('CartPole-v0'),
-        episodes=1000
-    )
-    experiment.run(agent(), label=agent_name + " " + rev)
+from runner import run
 
 if __name__ == '__main__':
-    run()
+    run(classic_control, GymEnvironment('CartPole-v0'), episodes=1000)
--- benchmarks/pong.py
+++ benchmarks/pong.py
@@ -1,32 +1,6 @@
-import subprocess
-import sys
 from all.environments import AtariEnvironment
-from all.experiments import Experiment
 from all.presets import atari
-
-def run():
-    try:
-        agent_name = sys.argv[1]
-    except IndexError:
-        print('Usage: python benchmarks/pong.py [agent_name]')
-        exit(1)
-
-    try:
-        agent = getattr(atari, agent_name)
-    except NameError:
-        print('Unknown agent: ' + agent_name)
-        exit(1)
-
-    # compute hash so that results can be exactly reconstructed
-    # by reverting to previous hash
-    result = subprocess.run(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE)
-    rev = result.stdout.decode('utf-8')
-
-    experiment = Experiment(
-        AtariEnvironment('Pong'),
-        frames=20e6
-    )
-    experiment.run(agent(), label=agent_name + " " + rev)
+from runner import run
 
 if __name__ == '__main__':
-    run()
+    run(atari, AtariEnvironment('Pong'), frames=20e6)
