8166e7d65ffcd8e5f9e08eb33e02e3064da564b9
Chris Nota <cpnota@gmail.com>
2019-04-05 15:09:01 -0400

    update benchmarks code

--- benchmarks/runner.py
+++ benchmarks/runner.py
@@ -1,32 +1,12 @@
 import subprocess
-import sys
-import torch
 from all.experiments import Experiment
 
-def run(presets, env, episodes=None, frames=None):
-    try:
-        agent_name = sys.argv[1]
-    except IndexError:
-        print('Usage: python benchmarks/[env].py [agent]')
-        exit(1)
-
-    try:
-        agent = getattr(presets, agent_name)
-    except NameError:
-        print('Unknown agent: ' + agent_name)
-        exit(1)
-
-
+def run(agent_name, agent, env, episodes=None, frames=None):
     result = subprocess.run(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE)
     rev = result.stdout.decode('utf-8')
-
-    device = torch.device('cuda')
-
-    env._device = device
-
     experiment = Experiment(
         env,
         frames=frames,
         episodes=episodes
     )
-    experiment.run(agent(device=device), label=agent_name + " " + rev)
+    experiment.run(agent, label=agent_name + " " + rev)
