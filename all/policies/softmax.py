fix softmax memory leak

all/policies/softmax.py
@@ -29,23 +29,14 @@ class SoftmaxPolicy(Policy):
     def reinforce(self, errors, retain_graph=False):
         # shape the data properly
         errors = errors.view(-1)
-        log_probs = torch.cat(self._log_probs)
-        entropy = torch.cat(self._entropy)
-
-        # adjust for batch size, fix cache
-        # TODO make sure we don't cause memory leaks
         batch_size = len(errors)
-        self._log_probs = [log_probs[batch_size:]]
-        log_probs = log_probs[:batch_size]
-        self._entropy = [entropy[batch_size:]]
-        entropy = entropy[:batch_size]
+        log_probs, entropy = self.decache(batch_size)
 
         # compute losses
         policy_loss = -torch.dot(log_probs, errors) / batch_size
         entropy_loss = -entropy.mean()
         loss = policy_loss + self.entropy_loss_scaling * entropy_loss
-        retain = retain_graph or len(log_probs) > 0
-        loss.backward(retain_graph=retain)
+        loss.backward(retain_graph=retain_graph)
 
         # take gradient steps
         if self.clip_grad != 0:
@@ -56,3 +47,19 @@ class SoftmaxPolicy(Policy):
     def cache(self, distribution, action):
         self._log_probs.append(distribution.log_prob(action))
         self._entropy.append(distribution.entropy())
+
+    def decache(self, batch_size):
+        i = 0
+        items = 0
+        while items < batch_size:
+            items += len(self._log_probs[i])
+            i += 1
+        if items != batch_size:
+            raise ValueError("Incompatible batch size.")
+        
+        log_probs = torch.cat(self._log_probs[:i])
+        self._log_probs = self._log_probs[i:]
+        entropy = torch.cat(self._entropy[:i])
+        self._entropy = self._entropy[i:]
+
+        return log_probs, entropy

all/policies/softmax_test.py
@@ -34,13 +34,13 @@ class TestSoftmax(unittest.TestCase):
         self.policy.reinforce(torch.tensor([[1, 2, 3]]).float())
 
     def test_multi_batch_reinforce(self):
-        states = torch.randn(10, STATE_DIM)
-        self.policy(states)
-        self.policy.reinforce(torch.tensor([1, 2, 3]).float())
+        self.policy(torch.randn(2, STATE_DIM))
+        self.policy(torch.randn(2, STATE_DIM))
+        self.policy(torch.randn(2, STATE_DIM))
         self.policy.reinforce(torch.tensor([1, 2, 3, 4]).float())
-        self.policy.reinforce(torch.tensor([1, 2, 3]).float())
+        self.policy.reinforce(torch.tensor([1, 2]).float())
         with self.assertRaises(Exception):
-            self.policy.reinforce(torch.tensor([1, 2, 3]).float())
+            self.policy.reinforce(torch.tensor([1, 2]).float())
 
     def test_list(self):
         torch.manual_seed(1)