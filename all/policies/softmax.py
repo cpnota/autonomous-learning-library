Fix softmax bug

all/policies/softmax.py
@@ -34,10 +34,15 @@ class SoftmaxPolicy(Policy):
         self.cache(distribution, action)
         return distribution.log_prob(action).exp().detach()
 
-    def eval(self, state):
+    def eval(self, state, action=None):
         with torch.no_grad():
             scores = self.model(state)
-            return functional.softmax(scores, dim=-1)
+            probs = functional.softmax(scores, dim=-1)
+            distribution = torch.distributions.Categorical(probs)
+            if action is None:
+                action = distribution.sample()
+                return action
+            return distribution.log_prob(action).exp().detach()
 
     def reinforce(self, errors, retain_graph=False):
         # shape the data properly
@@ -60,14 +65,13 @@ class SoftmaxPolicy(Policy):
             self.optimizer.zero_grad()
 
     def cache(self, distribution, action):
-        if distribution.probs.requires_grad:
-            self._log_probs.append(distribution.log_prob(action))
-            self._entropy.append(distribution.entropy())
+        self._log_probs.append(distribution.log_prob(action))
+        self._entropy.append(distribution.entropy())
 
     def decache(self, batch_size):
         i = 0
         items = 0
-        while items < batch_size:
+        while items < batch_size and i < len(self._log_probs):
             items += len(self._log_probs[i])
             i += 1
         if items != batch_size: