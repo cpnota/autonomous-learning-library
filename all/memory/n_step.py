78cb5924962879e10e70201f12448f2fdd76229c
Chris Nota <cpnota@gmail.com>
2019-05-14 15:05:02 -0400

    add NStepBatchBuffer

--- all/memory/n_step.py
+++ all/memory/n_step.py
@@ -2,6 +2,7 @@ import torch
 
 
 class NStepBuffer():
+    '''A rolling n-step buffer, such that every sample is an n-step rollout.'''
     def __init__(self, n, discount_factor=1):
         self.n = n
         self.gamma = discount_factor
@@ -76,3 +77,64 @@ class NStepBuffer():
         self._rewards.append(reward)
         self._next_states.append(next_state)
         self._lengths.append(length)
+
+class NStepBatchBuffer():
+    '''
+    A batch version of the n-step buffer, such that the first sample is n-step, second is n-1-step, etc.
+    This may be better for on-policy methods.
+    '''
+    def __init__(self, n, batch_size, discount_factor=1):
+        self.n = n
+        self.batch_size = batch_size
+        self.gamma = discount_factor
+        self.i = 0
+        self._states = []
+        self._rewards = []
+
+    def __len__(self):
+        return len(self._states)
+
+    def store(self, states, rewards):
+        if self.i == 0:
+            self._states = [states]
+            self._rewards = [rewards]
+            self.i = 1
+        elif self.i <= self.batch_size:
+            self._states.append(states)
+            self._rewards.append(rewards)
+            self.i += 1
+        else:
+            raise Exception("Buffer length exceeded: " + self.n)
+
+    def sample(self, _):
+        if self.i <= self.batch_size:
+            raise Exception("Not enough states received!")
+
+        n_envs = len(self._states[0])
+        sample_n = n_envs * self.batch_size
+        sample_states = [None] * sample_n
+        sample_next_states = [None] * sample_n
+        sample_returns = torch.zeros(sample_n, device=self._rewards[0].device)
+
+        # compute the N-step returns the slow way
+        for e in range(n_envs):
+            for t in range(self.batch_size):
+                i = t * n_envs + e
+                state = self._states[t][e]
+                returns = 0.
+                next_state = None
+                if state is not None:
+                    for k in range(1, self.n + 1):
+                        next_state = self._states[t + k][e]
+                        returns += (self.gamma ** (k - 1)) * \
+                            self._rewards[t + k][e]
+                        if next_state is None or t + k == self.batch_size:
+                            break
+                sample_states[i] = state
+                sample_next_states[i] = next_state
+                sample_returns[i] = returns
+
+        self._states = [self._states[-1]]
+        self._rewards = [self._rewards[-1]]
+        self.i = 1
+        return (sample_states, sample_next_states, sample_returns)
