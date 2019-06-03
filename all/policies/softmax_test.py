fix softmax tests

all/policies/softmax_test.py
@@ -2,6 +2,7 @@ import unittest
 import torch
 from torch import nn
 import torch_testing as tt
+from all.environments import State
 from all.policies import SoftmaxPolicy
 
 STATE_DIM = 2
@@ -17,10 +18,10 @@ class TestSoftmax(unittest.TestCase):
         self.policy = SoftmaxPolicy(self.model, optimizer, ACTIONS)
 
     def test_run(self):
-        state = torch.randn(1, STATE_DIM)
+        state = State(torch.randn(1, STATE_DIM))
         action = self.policy(state)
         self.assertEqual(action.item(), 0)
-        state = torch.randn(1, STATE_DIM)
+        state = State(torch.randn(1, STATE_DIM))
         action = self.policy(state)
         self.assertEqual(action.item(), 2)
         self.policy.reinforce(torch.tensor([-1, 1000000]).float())
@@ -28,15 +29,15 @@ class TestSoftmax(unittest.TestCase):
         self.assertEqual(action.item(), 2)
 
     def test_multi_action(self):
-        states = torch.randn(3, STATE_DIM)
+        states = State(torch.randn(3, STATE_DIM))
         actions = self.policy(states)
         tt.assert_equal(actions, torch.tensor([2, 2, 0]))
         self.policy.reinforce(torch.tensor([[1, 2, 3]]).float())
 
     def test_multi_batch_reinforce(self):
-        self.policy(torch.randn(2, STATE_DIM))
-        self.policy(torch.randn(2, STATE_DIM))
-        self.policy(torch.randn(2, STATE_DIM))
+        self.policy(State(torch.randn(2, STATE_DIM)))
+        self.policy(State(torch.randn(2, STATE_DIM)))
+        self.policy(State(torch.randn(2, STATE_DIM)))
         self.policy.reinforce(torch.tensor([1, 2, 3, 4]).float())
         self.policy.reinforce(torch.tensor([1, 2]).float())
         with self.assertRaises(Exception):
@@ -44,26 +45,19 @@ class TestSoftmax(unittest.TestCase):
 
     def test_list(self):
         torch.manual_seed(1)
-        states = [
-            torch.randn(1, STATE_DIM),
-            None,
-            torch.randn(1, STATE_DIM)
-        ]
+        states = State(torch.randn(3, STATE_DIM), torch.tensor([1, 0, 1]))
         actions = self.policy(states)
-        tt.assert_equal(actions, torch.tensor([2, 0, 1]))
+        tt.assert_equal(actions, torch.tensor([1, 2, 1]))
         self.policy.reinforce(torch.tensor([[1, 2, 3]]).float())
 
     def test_action_prob(self):
         torch.manual_seed(1)
-        states = [
-            torch.randn(1, STATE_DIM),
-            None,
-            torch.randn(1, STATE_DIM)
-        ]
+        states = State(torch.randn(3, STATE_DIM), torch.tensor([1, 0, 1]))
         with torch.no_grad():
             actions = self.policy(states)
         probs = self.policy(states, action=actions)
-        tt.assert_almost_equal(probs, torch.tensor([0.4814, 0.3333, 0.2049]), decimal=3)
+        tt.assert_almost_equal(probs, torch.tensor([0.204, 0.333, 0.217]), decimal=3)
+
 
 if __name__ == '__main__':
     unittest.main()