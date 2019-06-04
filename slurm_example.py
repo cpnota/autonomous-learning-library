4fe0f70a08c246c8ae0f101f68f05698f1d3c3c1
Chris Nota <cpnota@gmail.com>
2019-06-04 17:05:03 -0400

    run for many frames slurm

--- slurm_example.py
+++ slurm_example.py
@@ -1,4 +1,3 @@
-import torch
 from gym import envs
 from all.experiments import SlurmExperiment
 from all.presets.atari import a2c
@@ -7,4 +6,4 @@ from all.environments import GymEnvironment
 device = 'cuda'
 envs = [GymEnvironment(env, device) for env in envs.registry.env_specs.keys() if 'NoFrameskip-v4' in env and not '-ram' in env]
 # envs = [AtariEnvironment(env, device) for env in ['Pong', 'Breakout', 'SpaceInvaders']]
-SlurmExperiment(a2c, envs, 50000, hyperparameters={'device': device})
+SlurmExperiment(a2c, envs, 1e9, hyperparameters={'device': device})
