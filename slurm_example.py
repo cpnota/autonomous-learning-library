6debf3273da14a2d70881bc81e361bfa0f556240
Chris Nota <cpnota@gmail.com>
2019-06-04 15:35:57 -0400

    use cuda

--- slurm_example.py
+++ slurm_example.py
@@ -1,7 +1,8 @@
+import torch
 from all.experiments import SlurmExperiment
 from all.presets.atari import a2c
 from all.environments import AtariEnvironment
 
-envs = [AtariEnvironment(env) for env in ['Pong', 'Breakout', 'SpaceInvaders']]
-
-SlurmExperiment(a2c, envs, 50000)
+device = torch.device('cuda')
+envs = [AtariEnvironment(env, device) for env in ['Pong', 'Breakout', 'SpaceInvaders']]
+SlurmExperiment(a2c, envs, 50000, hyperparameters={'device': device})
