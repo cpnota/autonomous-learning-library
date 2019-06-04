28aadb07defc71a9370c27132540b68e05a5b1c4
Chris Nota <cpnota@gmail.com>
2019-06-04 16:54:53 -0400

    run all atari games

--- slurm_example.py
+++ slurm_example.py
@@ -1,8 +1,10 @@
 import torch
+from gym import envs
 from all.experiments import SlurmExperiment
 from all.presets.atari import a2c
-from all.environments import AtariEnvironment
+from all.environments import GymEnvironment
 
 device = torch.device('cuda')
-envs = [AtariEnvironment(env, device) for env in ['Pong', 'Breakout', 'SpaceInvaders']]
+envs = [GymEnvironment(env, device) for env in envs.registry.env_specs.keys() if 'NoFrameskip-v4' in env and not '-ram' in env]
+# envs = [AtariEnvironment(env, device) for env in ['Pong', 'Breakout', 'SpaceInvaders']]
 SlurmExperiment(a2c, envs, 50000, hyperparameters={'device': device})
