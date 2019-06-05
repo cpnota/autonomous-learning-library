2918bd1bf8e45cc5001a876cdb2cc8938dae2429
Chris Nota <cpnota@gmail.com>
2019-06-05 13:35:09 -0400

    make sarsa use only act

--- all/agents/sarsa.py
+++ all/agents/sarsa.py
@@ -7,27 +7,18 @@ class Sarsa(Agent):
         self.policy = policy
         self.gamma = gamma
         self.env = None
-        self.state = None
-        self.action = None
-        self.next_state = None
-        self.next_action = None
+        self.previous_state = None
+        self.previous_action = None
 
-    def initial(self, state):
-        self.state = state
-        self.action = self.policy(self.state)
-        return self.action
-
-    def act(self, next_state, reward):
-        next_action = self.policy(next_state)
-        td_error = (
-            reward
-            + self.gamma * self.q.eval(next_state, next_action)
-            - self.q(self.state, self.action)
-        )
-        self.q.reinforce(td_error)
-        self.state = next_state
-        self.action = next_action
-        return self.action
-
-    def terminal(self, state, reward):
-        return self.act(state, reward)
+    def act(self, state, reward):
+        action = self.policy(state)
+        if self.previous_state:
+            td_error = (
+                reward
+                + self.gamma * self.q.eval(state, action)
+                - self.q(self.previous_state, self.previous_action)
+            )
+            self.q.reinforce(td_error)
+        self.previous_state = state
+        self.previous_action = action
+        return action
