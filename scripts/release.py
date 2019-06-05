3aee1c77fd79cb3b53da255c109ab63976de53bb
Chris Nota <cpnota@gmail.com>
2019-06-05 11:11:17 -0400

    move benchmarks folder to scripts

---
+++ scripts/release.py
@@ -0,0 +1,26 @@
+'''Create slurm tasks to run benchmark suite'''
+import argparse
+from all.environments import AtariEnvironment, GymEnvironment
+from all.experiments import SlurmExperiment
+from all.presets import atari, classic_control
+
+# run on gpu
+device = 'cuda'
+
+# create slurm tasks for running classic control agents
+for agent_name in classic_control.__all__:
+    print('CartPole-v0,', agent_name)
+    agent = getattr(classic_control, agent_name)
+    envs = [GymEnvironment('CartPole-v0', device=device)]
+    SlurmExperiment(agent, envs, 100000, hyperparameters={'device': device}, sbatch_args={
+        'partition': '1080ti-short'
+    })
+
+# create slurm tasks for running atari agents
+for agent_name in atari.__all__:
+    print('Breakout', agent_name)
+    agent = getattr(atari, agent_name)
+    envs = [AtariEnvironment('Pong', device=device)]
+    SlurmExperiment(agent, envs, 2e7, hyperparameters={'device': device}, sbatch_args={
+        'partition': '1080ti-short'
+    })
