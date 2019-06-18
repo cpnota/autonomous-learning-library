356e6f6e3a67ca10d7e9c6b23917201b8e44e4a0
Chris Nota <cpnota@gmail.com>
2019-06-18 14:03:57 -0400

    create approximation base class

---
+++ all/approximation/approximation.py
@@ -0,0 +1,86 @@
+from abc import ABC, abstractmethod
+import copy
+import torch
+from torch import optim
+from torch.nn import utils
+from torch.nn.functional import mse_loss
+from all.experiments import DummyWriter
+
+class Approximation(ABC):
+    def __init__(
+            self,
+            model,
+            optimizer=None,
+            clip_grad=0,
+            loss_scaling=1,
+            loss=mse_loss,
+            name='approximation',
+            target_update_frequency=None,
+            writer=DummyWriter(),
+    ):
+        self.model = model
+        self.target_model = self._init_target_model(target_update_frequency)
+        self.device = next(model.parameters()).device
+        self._updates = 0
+        self._target_update_frequency = target_update_frequency
+        self._optimizer = optimizer or optim.Adam(model.parameters())
+        self._loss = loss
+        self._loss_scaling = loss_scaling
+        self._cache = []
+        self._clip_grad = clip_grad
+        self._writer = writer
+        self._name = name
+
+    @abstractmethod
+    def __call__(self, *inputs):
+        pass
+
+    @abstractmethod
+    def eval(self, *inputs):
+        pass
+
+    def reinforce(self, errors, retain_graph=False):
+        batch_size = len(errors)
+        cache = self._dequeue(batch_size)
+        if cache.requires_grad:
+            loss = self._loss(cache, errors) * self._loss_scaling
+            self._writer.add_loss(self._name, loss)
+            loss.backward(retain_graph=retain_graph)
+            if self._clip_grad != 0:
+                utils.clip_grad_norm_(self.model.parameters(), self._clip_grad)
+            self._optimizer.step()
+            self._optimizer.zero_grad()
+            self._update_target_model()
+
+    def _enqueue(self, results):
+        self._cache.append(results)
+
+    def _dequeue(self, batch_size):
+        i = 0
+        num_items = 0
+        while num_items < batch_size and i < len(self._cache):
+            num_items += len(self._cache[i])
+            i += 1
+        if num_items != batch_size:
+            raise ValueError("Incompatible batch size.")
+        items = torch.cat(self._cache[:i])
+        self._cache = self._cache[i:]
+        return items
+
+    def _init_target_model(self, target_update_frequency):
+        return (
+            copy.deepcopy(self.model)
+            if target_update_frequency is not None
+            else self.model
+        )
+
+    def _update_target_model(self):
+        self._updates += 1
+        if self._should_update_target():
+            self.target_model.load_state_dict(self.model.state_dict())
+
+    def _should_update_target(self):
+        return (
+            (self._target_update_frequency is not None)
+            and (self._updates % self._target_update_frequency == 0)
+        )
--- all/approximation/q_network.py
+++ all/approximation/q_network.py
@@ -1,42 +1,36 @@
-import copy
 import torch
-from torch import optim
 from torch.nn.functional import mse_loss
-from all.experiments import DummyWriter
 from all.layers import ListNetwork
-from .q_function import QFunction
+from .approximation import Approximation
 
+def td_loss(loss):
+    def _loss(estimates, errors):
+        return loss(estimates, errors + estimates.detach())
+    return _loss
 
-class QNetwork(QFunction):
+class QNetwork(Approximation):
     def __init__(
             self,
             model,
             optimizer,
-            actions,
+            num_actions,
             loss=mse_loss,
-            target_update_frequency=None,
-            writer=DummyWriter()
+            name='q',
+            **kwargs
     ):
-        self.model = ListNetwork(model, (actions,))
-        self.optimizer = (optimizer
-                          if optimizer is not None
-                          else optim.Adam(model.parameters()))
-        self.loss = loss
-        self.cache = None
-        self.updates = 0
-        self.target_update_frequency = target_update_frequency
-        self.target_model = (
-            copy.deepcopy(self.model)
-            if target_update_frequency is not None
-            else self.model
+        model = ListNetwork(model, (num_actions,))
+        loss = td_loss(loss)
+        super().__init__(
+            model,
+            optimizer,
+            loss=loss,
+            name=name,
+            **kwargs
         )
-        self.device = next(model.parameters()).device
-        self.writer = writer
 
     def __call__(self, states, actions=None):
         result = self._eval(states, actions, self.model)
-        if result.requires_grad:
-            self.cache = result
+        self._enqueue(result)
         return result.detach()
 
     def eval(self, states, actions=None):
@@ -46,17 +40,6 @@ class QNetwork(QFunction):
             self.target_model.train(training)
             return result
 
-    def reinforce(self, td_errors):
-        targets = td_errors + self.cache.detach()
-        loss = self.loss(self.cache, targets)
-        self.writer.add_loss('q', loss)
-        loss.backward()
-        self.optimizer.step()
-        self.optimizer.zero_grad()
-        self.updates += 1
-        if self.should_update_target():
-            self.target_model.load_state_dict(self.model.state_dict())
-
     def _eval(self, states, actions, model):
         values = model(states)
         if actions is None:
@@ -64,9 +47,3 @@ class QNetwork(QFunction):
         if isinstance(actions, list):
             actions = torch.tensor(actions, device=self.device)
         return values.gather(1, actions.view(-1, 1)).squeeze(1)
-
-    def should_update_target(self):
-        return (
-            (self.target_update_frequency is not None)
-            and (self.updates % self.target_update_frequency == 0)
-        )
