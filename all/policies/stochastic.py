5e67657e74c8ced39b163512b9a09f7c2f55f331
Chris Nota <cpnota@gmail.com>
2019-07-07 14:03:48 -0400

    add StochasticPolicy base class

--- all/policies/stochastic.py
+++ all/policies/stochastic.py
@@ -1,44 +1,45 @@
 import torch
-from torch.nn import functional, utils
+from torch.nn import utils
 from all.experiments import DummyWriter
-from all.layers import ListNetwork
 from .policy import Policy
 
 
-class SoftmaxPolicy(Policy):
+class StochasticPolicy(Policy):
     def __init__(
             self,
             model,
             optimizer,
-            actions,
+            distribution,
+            name='policy',
             entropy_loss_scaling=0,
             clip_grad=0,
             writer=DummyWriter()
     ):
-        self.model = ListNetwork(model, (actions,))
+        self.model = model
         self.optimizer = optimizer
-        self.entropy_loss_scaling = entropy_loss_scaling
-        self.clip_grad = clip_grad
+        self.distribution = distribution
+        self.name = name
+        self.device = next(model.parameters()).device
+        self._entropy_loss_scaling = entropy_loss_scaling
+        self._clip_grad = clip_grad
         self._log_probs = []
         self._entropy = []
         self._writer = writer
 
     def __call__(self, state, action=None, prob=None):
-        scores = self.model(state)
-        probs = functional.softmax(scores, dim=-1)
-        distribution = torch.distributions.Categorical(probs)
+        outputs = self.model(state)
+        distribution = self.distribution(outputs)
         if action is None:
             action = distribution.sample()
-            self.cache(distribution, action)
+            self._cache(distribution, action)
             return action
-        self.cache(distribution, action)
+        self._cache(distribution, action)
         return distribution.log_prob(action).exp().detach()
 
     def eval(self, state, action=None):
         with torch.no_grad():
-            scores = self.model(state)
-            probs = functional.softmax(scores, dim=-1)
-            distribution = torch.distributions.Categorical(probs)
+            outputs = self.model(state)
+            distribution = self.distribution(outputs)
             if action is None:
                 action = distribution.sample()
                 return action
@@ -48,27 +49,28 @@ class SoftmaxPolicy(Policy):
         # shape the data properly
         errors = errors.view(-1)
         batch_size = len(errors)
-        log_probs, entropy = self.decache(batch_size)
+        log_probs, entropy = self._decache(batch_size)
         if log_probs.requires_grad:
             # compute losses
-            policy_loss = -torch.dot(log_probs, errors) / batch_size
+            policy_loss = (-log_probs.transpose(0, -1) * errors).mean()
             entropy_loss = -entropy.mean()
-            self._writer.add_loss('policy', policy_loss)
-            self._writer.add_loss('entropy', entropy_loss)
-            loss = policy_loss + self.entropy_loss_scaling * entropy_loss
+            loss = policy_loss + self._entropy_loss_scaling * entropy_loss
+            self._writer.add_loss(self.name, loss)
+            self._writer.add_loss(self.name + '/pg', policy_loss)
+            self._writer.add_loss(self.name + '/entropy', entropy_loss)
             loss.backward(retain_graph=retain_graph)
 
             # take gradient steps
-            if self.clip_grad != 0:
-                utils.clip_grad_norm_(self.model.parameters(), self.clip_grad)
+            if self._clip_grad != 0:
+                utils.clip_grad_norm_(self.model.parameters(), self._clip_grad)
             self.optimizer.step()
             self.optimizer.zero_grad()
 
-    def cache(self, distribution, action):
+    def _cache(self, distribution, action):
         self._log_probs.append(distribution.log_prob(action))
         self._entropy.append(distribution.entropy())
 
-    def decache(self, batch_size):
+    def _decache(self, batch_size):
         i = 0
         items = 0
         while items < batch_size and i < len(self._log_probs):
