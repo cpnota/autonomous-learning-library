e0bfd251039ee3717ab3fc60ab5f464ad06cc96e
Chris Nota <cpnota@gmail.com>
2019-07-12 15:09:47 -0400

    update release script

--- scripts/release.py
+++ scripts/release.py
@@ -2,7 +2,7 @@
 import argparse
 from all.environments import AtariEnvironment, GymEnvironment
 from all.experiments import SlurmExperiment
-from all.presets import atari, classic_control
+from all.presets import atari, classic_control, continuous
 
 # run on gpu
 device = 'cuda'
@@ -23,4 +23,13 @@ for agent_name in atari.__all__:
     envs = [AtariEnvironment('Breakout', device=device)]
     SlurmExperiment(agent, envs, 2e7, hyperparameters={'device': device}, sbatch_args={
         'partition': '1080ti-long'
+    })
+
+# create slurm tasks for running atari agents
+for agent_name in continuous.__all__:
+    print('Pendulum-v0', agent_name)
+    agent = getattr(continuous, agent_name)
+    envs = [GymEnvironment('Pendulum-v0', device=device)]
+    SlurmExperiment(agent, envs, 500000, hyperparameters={'device': device}, sbatch_args={
+        'partition': '1080ti-short'
     })
