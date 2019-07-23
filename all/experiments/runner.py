54d91a003b3837f7bf8d20d780064b6d5e9c67bb
Chris Nota <cpnota@gmail.com>
2019-07-23 10:42:16 -0400

    re-add parallel runner

--- all/experiments/runner.py
+++ all/experiments/runner.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from timeit import default_timer as timer
 import numpy as np
+import torch
+from all.environments import State
 from .writer import ExperimentWriter
 
 class EnvRunner(ABC):
@@ -8,14 +10,17 @@ class EnvRunner(ABC):
             self,
             agent,
             env,
+            agent_name=None,
+            env_name=None,
             frames=np.inf,
             episodes=np.inf,
             render=False,
             quiet=False,
             write_loss=True,
     ):
-        self._label = agent.__name__
         self._env = env
+        self._agent_name = agent_name or agent.__name__
+        self._env_name = env_name or self._env.name
         self._max_frames = frames
         self._max_episodes = episodes
         self._render = render
@@ -43,7 +48,7 @@ class EnvRunner(ABC):
         self._writer.add_scalar('fps', fps, step="frame")
 
     def _make_writer(self, write_loss):
-        return ExperimentWriter(self._label, self._env.name, loss=write_loss)
+        return ExperimentWriter(self._agent_name, self._env_name, loss=write_loss)
 
 class SingleEnvRunner(EnvRunner):
     def run(self):
@@ -78,5 +83,52 @@ class SingleEnvRunner(EnvRunner):
         return returns
 
 class ParallelEnvRunner(EnvRunner):
+    def __init__(self, agent, env, **kwargs):
+        make_agent, n_envs = agent
+        envs = env.duplicate(n_envs)
+        self._n_envs = n_envs
+        self._returns = None
+        self._start_time = None
+        super().__init__(make_agent, envs, env_name=env.name, **kwargs)
+
     def run(self):
-        pass
+        self._reset()
+        while not self._done():
+            self._step()
+
+    def _reset(self):
+        for env in self._env:
+            env.reset()
+        self._returns = torch.zeros(
+            (self._n_envs),
+            dtype=torch.float,
+            device=self._env[0].device
+        )
+        self._start_time = timer()
+
+    def _step(self):
+        states = State.from_list([env.state for env in self._env])
+        rewards = torch.tensor(
+            [env.reward for env in self._env],
+            dtype=torch.float,
+            device=self._env[0].device
+        )
+        actions = self._agent.act(states, rewards)
+
+        for i, env in enumerate(self._env):
+            self._step_env(i, env, actions[i])
+
+    def _step_env(self, i, env, action):
+        if env.done:
+            self._returns[i] += env.reward
+            end_time = timer()
+            fps = self._writer.frames / (end_time - self._start_time)
+            self._log(self._returns[i], fps)
+            env.reset()
+            self._returns[i] = 0
+            self._writer.episodes += 1
+        else:
+            if action is not None:
+                self._returns[i] += env.reward
+                env.step(action)
+                self._writer.frames += 1
