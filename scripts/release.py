8723d041634955f027dced292f015d94c416fadf
Chris Nota <cpnota@gmail.com>
2019-07-23 11:39:04 -0400

    update scripts

--- scripts/release.py
+++ scripts/release.py
@@ -7,29 +7,33 @@ from all.presets import atari, classic_control, continuous
 # run on gpu
 device = 'cuda'
 
-# create slurm tasks for running classic control agents
-for agent_name in classic_control.__all__:
-    print('CartPole-v0,', agent_name)
-    agent = getattr(classic_control, agent_name)
-    envs = [GymEnvironment('CartPole-v0', device=device)]
-    SlurmExperiment(agent, envs, 100000, hyperparameters={'device': device}, sbatch_args={
-        'partition': '1080ti-short'
-    })
+def get_agents(preset):
+    agents = [getattr(preset, agent_name) for agent_name in classic_control.__all__]
+    return [agent(device=device) for agent in agents]
 
-# create slurm tasks for running atari agents
-for agent_name in atari.__all__:
-    print('Breakout', agent_name)
-    agent = getattr(atari, agent_name)
-    envs = [AtariEnvironment('Breakout', device=device)]
-    SlurmExperiment(agent, envs, 2e7, hyperparameters={'device': device}, sbatch_args={
+SlurmExperiment(
+    get_agents(atari),
+    AtariEnvironment('Breakout', device=device),
+    2e7,
+    sbatch_args={
         'partition': '1080ti-long'
-    })
+    }
+)
 
-# create slurm tasks for running atari agents
-for agent_name in continuous.__all__:
-    print('Lander', agent_name)
-    agent = getattr(continuous, agent_name)
-    envs = [GymEnvironment('LunarLanderContinuous-v2', device=device)]
-    SlurmExperiment(agent, envs, 500000, hyperparameters={'device': device}, sbatch_args={
+SlurmExperiment(
+    get_agents(classic_control),
+    GymEnvironment('CartPole-v0', device=device),
+    100000,
+    sbatch_args={
         'partition': '1080ti-short'
-    })
+    }
+)
+
+SlurmExperiment(
+    get_agents(continuous),
+    GymEnvironment('LunarLanderContinuous-v2', device=device),
+    500000,
+    sbatch_args={
+        'partition': '1080ti-short'
+    }
+)
