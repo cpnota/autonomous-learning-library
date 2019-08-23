66bbddf19ee754c6800235330db108e84628ed16
Chris Nota <cpnota@gmail.com>
2019-08-23 17:33:38 -0400

    add assertion to make sure priority is non-infinite

--- all/memory/segment_tree.py
+++ all/memory/segment_tree.py
@@ -113,7 +113,6 @@ class SumSegmentTree(SegmentTree):
         idx: int
             highest index satisfying the prefixsum constraint
         """
-        assert 0 <= prefixsum <= self.sum() + 1e-5
         idx = 1
         while idx < self._capacity:  # while non-leaf
             if self._value[2 * idx] > prefixsum:
