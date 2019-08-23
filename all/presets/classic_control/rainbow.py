00b80011696c8eac676316c1f0cf8561083af76a
Chris Nota <cpnota@gmail.com>
2019-08-23 15:11:00 -0400

    Rainbow working great on CartPole

--- all/presets/classic_control/rainbow.py
+++ all/presets/classic_control/rainbow.py
@@ -2,10 +2,12 @@
 import torch
 from torch.optim import Adam
 from all.agents import C51
-from all.approximation import QDist, PolyakTarget
+from all.approximation import QDist
 from all.logging import DummyWriter
-from all.memory import PrioritizedReplayBuffer, NStepReplayBuffer, ExperienceReplayBuffer
-from all.optim import LinearScheduler
+from all.memory import (
+    PrioritizedReplayBuffer,
+    NStepReplayBuffer,
+)
 from .models import fc_relu_rainbow
 
 
@@ -21,17 +23,14 @@ def rainbow(
         # prioritized replay
         alpha=0.2,  # priority scaling
         beta=0.6,  # importance sampling adjustment
-        final_beta_frame=20000,
         # multi-step learning
         n_steps=5,
         # Distributional RL
         atoms=101,
         # Noisy Nets
-        sigma=1.,
-        # Polyak Target networks
-        polyak=0.001
+        sigma=0.5,
 ):
-    '''
+    """
     A complete implementation of Rainbow.
 
     The following enhancements have been applied:
@@ -41,44 +40,37 @@ def rainbow(
     4. Multi-step learning
     5. Distributional RL
     6. Noisy nets
-    7. Polyak target networks (not included in original rainbow!)
-    '''
+    """
+
     def _rainbow(env, writer=DummyWriter()):
         model = fc_relu_rainbow(env, atoms=atoms, sigma=sigma).to(device)
-        optimizer = Adam(model.parameters(), lr=lr, eps=1e-3)
+        optimizer = Adam(model.parameters(), lr=lr)
         q = QDist(
             model,
             optimizer,
             env.action_space.n,
             atoms,
-            v_min=-200,
-            v_max=200,
-            target=PolyakTarget(polyak),
+            v_min=-100,
+            v_max=100,
             writer=writer,
         )
+        # replay_buffer = ExperienceReplayBuffer(replay_buffer_size, device=device)
         replay_buffer = PrioritizedReplayBuffer(
             replay_buffer_size,
             alpha=alpha,
-            beta=LinearScheduler(
-                beta,
-                1,
-                0,
-                final_beta_frame,
-                name='beta',
-                writer=writer
-            ),
+            beta=beta,
             device=device
         )
         replay_buffer = NStepReplayBuffer(n_steps, discount_factor, replay_buffer)
         return C51(
             q,
             replay_buffer,
-            exploration=0.02,
+            exploration=0.,
             discount_factor=discount_factor ** n_steps,
             minibatch_size=minibatch_size,
             replay_start_size=replay_start_size,
             update_frequency=update_frequency,
-            writer=writer
+            writer=writer,
         )
 
     return _rainbow
