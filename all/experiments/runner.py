53c117ad03265e2b0ed1672210c0f3b174814fd5
Chris Nota <cpnota@gmail.com>
2019-08-24 15:11:52 -0400

    log rewards100

--- all/experiments/runner.py
+++ all/experiments/runner.py
@@ -23,6 +23,7 @@ class EnvRunner(ABC):
         self._render = render
         self._quiet = quiet
         self._best_returns = -np.inf
+        self._returns100 = []
         self.run()
 
     @abstractmethod
@@ -41,6 +42,11 @@ class EnvRunner(ABC):
                   (self._writer.episodes, self._writer.frames, fps, returns))
         if returns > self._best_returns:
             self._best_returns = returns
+        self._returns100.append(returns)
+        if len(self._returns100) == 100:
+            self._writer.add_evaluation('returns100/mean', np.mean(self._returns100), step="frame")
+            self._writer.add_evaluation('returns100/std', np.std(self._returns100), step="frame")
+            self._returns100 = []
         self._writer.add_evaluation('returns/episode', returns, step="episode")
         self._writer.add_evaluation('returns/frame', returns, step="frame")
         self._writer.add_evaluation("returns/max", self._best_returns, step="frame")
