fcfbe1dd0f9eb2cbd2a9ff98aa6d8343ebac989e
Chris Nota <cpnota@gmail.com>
2019-08-28 16:22:08 -0400

    add _should_explore method to c51

--- all/agents/c51.py
+++ all/agents/c51.py
@@ -5,6 +5,7 @@ from ._agent import Agent
 
 torch.set_printoptions(threshold=10000)
 
+
 class C51(Agent):
     """
     Implementation of C51, a categorical DQN agent
@@ -21,15 +22,15 @@ class C51(Agent):
     """
 
     def __init__(
-            self,
-            q_dist,
-            replay_buffer,
-            exploration=0.02,
-            discount_factor=0.99,
-            minibatch_size=32,
-            replay_start_size=5000,
-            update_frequency=1,
-            writer=DummyWriter()
+        self,
+        q_dist,
+        replay_buffer,
+        exploration=0.02,
+        discount_factor=0.99,
+        minibatch_size=32,
+        replay_start_size=5000,
+        update_frequency=1,
+        writer=DummyWriter(),
     ):
         # objects
         self.q_dist = q_dist
@@ -60,7 +61,7 @@ class C51(Agent):
             self.replay_buffer.store(self.state, self.action, reward, state)
 
     def _choose_action(self, state):
-        if np.random.rand() < self.exploration:
+        if self._should_explore():
             return torch.randint(
                 self.q_dist.n_actions, (len(state),), device=self.q_dist.device
             )
@@ -68,16 +69,22 @@ class C51(Agent):
 
     def _train(self):
         if self._should_train():
-            (states, actions, rewards, next_states, weights) = self.replay_buffer.sample(
-                self.minibatch_size
-            )
+            (
+                states,
+                actions,
+                rewards,
+                next_states,
+                weights,
+            ) = self.replay_buffer.sample(self.minibatch_size)
             actions = torch.cat(actions)
             # choose best action from online network, double-q style
             next_actions = self._best_actions(next_states)
             # compute the distribution at the next state
             next_dist = self.q_dist.target(next_states, next_actions)
             # shift the atoms in the next distribution
-            shifted_atoms = (rewards.view((-1, 1)) + self.discount_factor * self.q_dist.atoms)
+            shifted_atoms = (
+                rewards.view((-1, 1)) + self.discount_factor * self.q_dist.atoms
+            )
             # project the disribution back on the original set of atoms
             target_dist = self.q_dist.project(next_dist, shifted_atoms)
             # apply update
@@ -86,17 +93,25 @@ class C51(Agent):
             loss.backward()
             self.q_dist.step()
             # useful for debugging
-            self.writer.add_loss('q_dist', loss.detach())
-            self.writer.add_loss('q_mean', (dist.detach() * self.q_dist.atoms).sum(dim=1).mean())
+            self.writer.add_loss("q_dist", loss.detach())
+            self.writer.add_loss(
+                "q_mean", (dist.detach() * self.q_dist.atoms).sum(dim=1).mean()
+            )
 
     def _best_actions(self, states):
         probs = self.q_dist.eval(states)
         q_values = (probs * self.q_dist.atoms).sum(dim=2)
         return torch.argmax(q_values, dim=1)
 
+    def _should_explore(self):
+        return (
+            len(self.replay_buffer) < self.replay_start_size
+            or np.random.rand() < self.exploration
+        )
+
     def _should_train(self):
         return (
-            self.frames_seen > self.replay_start_size
+            len(self.replay_buffer) >= self.replay_start_size
             and self.frames_seen % self.update_frequency == 0
         )
 
