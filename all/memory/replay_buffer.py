06ff3128c0d9e891bd296931205bb91fbbe8ff93
Chris Nota <cpnota@gmail.com>
2019-09-05 11:22:37 -0400

    make replay buffer use numpy random

--- all/memory/replay_buffer.py
+++ all/memory/replay_buffer.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-import random
 import numpy as np
 import torch
 from all.environments import State
@@ -131,7 +130,7 @@ class PrioritizedReplayBuffer(ExperienceReplayBuffer, Schedulable):
         p_total = self._it_sum.sum(0, len(self) - 1)
         every_range_len = p_total / batch_size
         for i in range(batch_size):
-            mass = random.random() * every_range_len + i * every_range_len
+            mass = np.random.random() * every_range_len + i * every_range_len
             idx = self._it_sum.find_prefixsum_idx(mass)
             res.append(idx)
         return res
