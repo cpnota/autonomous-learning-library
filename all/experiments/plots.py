227111ba22f7cc775b54521761d97cd3b6ce2094
Chris Nota <cpnota@gmail.com>
2019-09-11 15:43:09 -0400

    refactor plots

--- all/experiments/plots.py
+++ all/experiments/plots.py
@@ -1,8 +1,7 @@
-import csv
-import json
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def plot_returns_100(runs_dir):
     data = load_returns_100_data(runs_dir)
@@ -11,23 +10,8 @@ def plot_returns_100(runs_dir):
     fig, axes = plt.subplots(1, len(data))
     for i, env in enumerate(data):
         ax = axes[i]
-        for agent in data[env]:
-            agent_data = data[env][agent]
-            timesteps = agent_data[0]
-            mean = np.array(agent_data[1])
-            std = np.array(agent_data[2])
-
-            if agent in lines:
-                ax.plot(timesteps, mean, label=agent, color=lines[agent].get_color())
-            else:
-                line, = ax.plot(timesteps, mean, label=agent)
-                lines[agent] = line
-            ax.fill_between(timesteps, mean + std, mean - std, alpha=0.2, color=lines[agent].get_color())
-            ax.set_title(env)
-            ax.set_xlabel('timesteps')
-
-    # create legend
-    fig.legend(list(lines.values()), list(lines.keys()), loc='center right')
+        subplot_returns_100(ax, env, data[env], lines)
+    fig.legend(list(lines.values()), list(lines.keys()), loc="center right")
     plt.show()
 
 
@@ -37,17 +21,33 @@ def load_returns_100_data(runs_dir):
     def add_data(agent, env, file):
         if not env in data:
             data[env] = {}
-        data[env][agent] = np.genfromtxt(file, delimiter=',')
+        data[env][agent] = np.genfromtxt(file, delimiter=",").reshape((-1, 3))
 
     for agent_dir in os.listdir(runs_dir):
-        agent = agent_dir.split(' ')[0].strip('_')
+        agent = agent_dir.split(" ")[0].strip("_")
         agent_path = os.path.join(runs_dir, agent_dir)
         if os.path.isdir(agent_path):
             for env in os.listdir(agent_path):
                 env_path = os.path.join(agent_path, env)
                 if os.path.isdir(env_path):
-                    returns100path = os.path.join(env_path, 'returns100.csv')
+                    returns100path = os.path.join(env_path, "returns100.csv")
                     if os.path.exists(returns100path):
                         add_data(agent, env, returns100path)
 
     return data
+
+def subplot_returns_100(ax, env, data, lines):
+    for agent in data:
+        agent_data = data[agent]
+        timesteps = agent_data[:, 0]
+        mean = agent_data[:, 1]
+        std = agent_data[:, 2]
+
+        if agent in lines:
+            ax.plot(timesteps, mean, label=agent, color=lines[agent].get_color())
+        else:
+            line, = ax.plot(timesteps, mean, label=agent)
+            lines[agent] = line
+        ax.fill_between(timesteps, mean + std, mean - std, alpha=0.2, color=lines[agent].get_color())
+        ax.set_title(env)
+        ax.set_xlabel("timesteps")
