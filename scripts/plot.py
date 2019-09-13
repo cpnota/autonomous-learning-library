02cec9df1b87bd1a81dfc2488ff4002771061d4a
Chris Nota <cpnota@gmail.com>
2019-09-13 18:47:42 -0400

    Feature/clean plots (#112)
    * make a2c use adam like everything elese
    
    * make plts use scientific notation for >100k frames
    
    * tweak a2c hyperparameters
    
    * add option to fix the endpoint of plots

--- scripts/plot.py
+++ scripts/plot.py
@@ -5,8 +5,9 @@ from all.experiments import plot_returns_100
 def plot():
     parser = argparse.ArgumentParser(description="Plots the results of experiments.")
     parser.add_argument("dir", help="Output directory.")
+    parser.add_argument("--timesteps", type=int, default=-1, help="The final point will be fixed to this x-value")
     args = parser.parse_args()
-    plot_returns_100(args.dir)
+    plot_returns_100(args.dir, timesteps=args.timesteps)
 
 if __name__ == "__main__":
     plot()
