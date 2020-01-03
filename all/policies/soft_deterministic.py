dfbea3af02b628a7d09c46fae4d5bf3526ae8988
Chris Nota <cpnota@gmail.com>
2020-01-02 21:27:40 -0500

    update sac

--- all/policies/soft_deterministic.py
+++ all/policies/soft_deterministic.py
@@ -2,7 +2,6 @@ import torch
 from all.approximation import Approximation
 from all.nn import ListNetwork
 
-
 class SoftDeterministicPolicy(Approximation):
     def __init__(
             self,
@@ -12,63 +11,57 @@ class SoftDeterministicPolicy(Approximation):
             name="policy",
             **kwargs
     ):
-        model = ListNetwork(model)
-        optimizer = optimizer
-        name = name
+        model = SoftDeterministicPolicyNetwork(model, space)
         super().__init__(model, optimizer, name=name, **kwargs)
+
+
+class SoftDeterministicPolicyNetwork(ListNetwork):
+    def __init__(self, model, space):
+        super().__init__(model)
+        self._space = space
         self._action_dim = space.shape[0]
-        self._entropy = []
-        self._last_dist = None
-        self._raw_actions = None
-        # parameters for squashing to tanh
-        self._tanh_scale = torch.tensor(
-            (space.high - space.low) / 2, device=self.device
-        )
-        self._tanh_mean = torch.tensor((space.high + space.low) / 2, device=self.device)
 
-    def __call__(self, state, action=None, log_prob=False):
-        outputs = self.model(state)
-        distribution = self._distribution(outputs)
-        raw_actions = distribution.rsample()
-        actions = self._squash(raw_actions)
-        if log_prob:
-            return (actions, self._log_prob(
-                distribution,
-                raw_actions,
-                actions
-            ))
-        return actions
-
-    def greedy(self, state):
-        return self._squash(self.model(state)[:, 0:self._action_dim])
-
-    def eval(self, state):
-        with torch.no_grad():
-            return self(state)
-
-    def target(self, state):
-        self._target(state)
-
-    def reinforce(self, _):
-        raise NotImplementedError(
-            "Deterministic policies are trainted through backpropagation."
-            + "Call backward() on a loss derived from the action"
-            + "and then call policy.step()"
-        )
-
-    def _distribution(self, outputs):
+    def forward(self, state):
+        outputs = super().forward(state)
         means = outputs[:, 0 : self._action_dim]
-        logvars = outputs[:, self._action_dim :]
+        logvars = outputs[:, self._action_dim:]
         std = logvars.mul(0.5).exp_()
-        self._last_dist = torch.distributions.normal.Normal(means, std)
-        return self._last_dist
+        return SquashedNormal(means, std, self._space)
+
+
+class SquashedNormal():
+    '''A normal distribution squashed through a tanh and rescaled'''
+    def __init__(self, loc, scale, space):
+        self._normal = torch.distributions.normal.Normal(loc, scale)
+        self._tanh_scale = torch.tensor((space.high - space.low) / 2, device=loc.device)
+        self._tanh_mean = torch.tensor((space.high + space.low) / 2, device=loc.device)
+
+    @property
+    def mean(self):
+        return self._squash(self._normal.mean)
+
+    def sample(self, **kwargs):
+        return self._squash(self._normal.sample(**kwargs))
+
+    def rsample(self, **kwargs):
+        raw = self._normal.rsample(**kwargs)
+        action = self._squash(raw)
+        log_prob = self._normal.log_prob(raw)
+        log_prob -= torch.log(1 - action.pow(2) + 1e-6)
+        log_prob = log_prob.sum(1)
+        return action, log_prob
+
+    def log_prob(self, action):
+        # Squashing using tanh changes the probabilty densities.
+        # See section C. of the Soft Actor-Critic appendix.
+        unsquashed = self._unsquash(action)
+        return self._normal.log_prob(unsquashed).sum(1) - torch.log(1 - torch.tanh(unsquashed) ** 2 + 1e-6).sum(1)
 
     def _squash(self, x):
         return torch.tanh(x) * self._tanh_scale + self._tanh_mean
 
-    def _log_prob(self, dist, raw, action):
-        # Squashing using tanh changes the probabilty densities.
-        # See section C. of the Soft Actor-Critic appendix.
-        log_prob = dist.log_prob(raw)
-        log_prob -= torch.log(1 - action.pow(2) + 1e-6)
-        return log_prob.sum(1)
+    def _unsquash(self, x):
+        return atanh((x - self._tanh_mean) / self._tanh_scale)
+
+def atanh(x):
+    return 0.5 * torch.log((1 + x) / (1 - x))
