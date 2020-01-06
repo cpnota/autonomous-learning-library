84108bac6f1bd1ac9c1270ed55fc571360213b2e
Chris Nota <cpnota@gmail.com>
2020-01-06 10:13:51 -0500

    more attempted improvements to ddpg

--- all/bodies/time.py
+++ all/bodies/time.py
@@ -3,14 +3,15 @@ from all.environments import State
 from ._body import Body
 
 class TimeFeature(Body):
-    def __init__(self, agent):
+    def __init__(self, agent, scale=0.001):
         self.timestep = None
+        self.scale = scale
         super().__init__(agent)
 
     def act(self, state, reward):
         if self.timestep is None:
-            self.timestep = torch.zeros(len(state))
-        features = torch.cat((state.features, self.timestep.view((-1, 1))), dim=1)
+            self.timestep = torch.zeros(len(state), device=state.features.device)
+        features = torch.cat((state.features, self.scale * self.timestep.view((-1, 1))), dim=1)
         state = State(features, state.mask, state.info)
         self.timestep = state.mask * (self.timestep + 1)
         return self.agent.act(state, reward)
--- all/presets/continuous/ddpg.py
+++ all/presets/continuous/ddpg.py
@@ -5,34 +5,55 @@ from torch.optim.lr_scheduler import CosineAnnealingLR
 from all import nn
 from all.agents import DDPG
 from all.approximation import QContinuous, PolyakTarget
+from all.bodies import TimeFeature
 from all.logging import DummyWriter
 from all.policies import DeterministicPolicy
 from all.memory import ExperienceReplayBuffer
 
 
-def fc_value(env):
+def fc_value(env, hidden1=400, hidden2=300):
     return nn.Sequential(
         nn.Flatten(),
-        nn.Linear(env.state_space.shape[0] + env.action_space.shape[0], 400),
+        nn.Linear(env.state_space.shape[0] + env.action_space.shape[0] + 1, hidden1),
         nn.ReLU(),
-        nn.Linear(400, 300),
+        nn.Linear(hidden1, hidden2),
         nn.ReLU(),
-        nn.Linear0(300, 1),
+        nn.Linear0(hidden2, 1),
     )
 
-def fc_policy(env):
+def fc_policy(env, hidden1=400, hidden2=300):
     return nn.Sequential(
         nn.Flatten(),
-        nn.Linear(env.state_space.shape[0], 400),
+        nn.Linear(env.state_space.shape[0] + 1, hidden1),
         nn.ReLU(),
-        nn.Linear(400, 300),
+        nn.Linear(hidden1, hidden2),
         nn.ReLU(),
-        nn.Linear0(300, env.action_space.shape[0]),
+        nn.Linear0(hidden2, env.action_space.shape[0]),
     )
+
+class Critic(nn.Module):
+    def __init__(self, env, hidden1=400, hidden2=300):
+        super().__init__()
+        self.action_dim = env.action_space.shape[0]
+        self.f = nn.Sequential(
+            nn.Linear(env.state_space.shape[0] + 1, hidden1),
+            nn.ReLU()
+        )
+        self.q = nn.Sequential(
+            nn.Linear(hidden1 + self.action_dim, hidden2),
+            nn.ReLU(),
+            nn.Linear0(hidden2, 1)
+        )
+
+    def forward(self, x):
+        state = x[:, 0:-self.action_dim]
+        action = x[:, -self.action_dim:]
+        return self.q(torch.cat((self.f(state), action), dim=1))
 
 def ddpg(
-        lr_q=3e-4,
+        lr_q=1e-3,
         lr_pi=1e-4,
+        weight_decay=1e-2,
         noise=0.2,
         replay_start_size=5000,
         replay_buffer_size=100000,
@@ -46,8 +67,8 @@ def ddpg(
     final_anneal_step = (final_frame - replay_start_size) // update_frequency
 
     def _ddpg(env, writer=DummyWriter()):
-        value_model = fc_value(env).to(device)
-        value_optimizer = Adam(value_model.parameters(), lr=lr_q)
+        value_model = Critic(env).to(device)
+        value_optimizer = Adam(value_model.parameters(), lr=lr_q, weight_decay=weight_decay)
         q = QContinuous(
             value_model,
             value_optimizer,
@@ -78,7 +99,7 @@ def ddpg(
             device=device
         )
 
-        return DDPG(
+        return TimeFeature(DDPG(
             q,
             policy,
             replay_buffer,
@@ -88,7 +109,7 @@ def ddpg(
             discount_factor=discount_factor,
             update_frequency=update_frequency,
             minibatch_size=minibatch_size,
-        )
+        ))
     return _ddpg
 
 
