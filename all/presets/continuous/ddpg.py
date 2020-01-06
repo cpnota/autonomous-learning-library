e029c87a6a3fa7b658c667d372f80057e333ffac
Chris Nota <cpnota@gmail.com>
2020-01-06 16:55:49 -0500

    ddpg

--- all/presets/continuous/ddpg.py
+++ all/presets/continuous/ddpg.py
@@ -31,46 +31,26 @@ def fc_policy(env, hidden1=400, hidden2=300):
         nn.Linear0(hidden2, env.action_space.shape[0]),
     )
 
-class Critic(nn.Module):
-    def __init__(self, env, hidden1=400, hidden2=300):
-        super().__init__()
-        self.action_dim = env.action_space.shape[0]
-        self.f = nn.Sequential(
-            nn.Linear(env.state_space.shape[0] + 1, hidden1),
-            nn.ReLU()
-        )
-        self.q = nn.Sequential(
-            nn.Linear(hidden1 + self.action_dim, hidden2),
-            nn.ReLU(),
-            nn.Linear0(hidden2, 1)
-        )
-
-    def forward(self, x):
-        state = x[:, 0:-self.action_dim]
-        action = x[:, -self.action_dim:]
-        return self.q(torch.cat((self.f(state), action), dim=1))
-
 def ddpg(
         lr_q=1e-3,
-        lr_pi=1e-4,
-        weight_decay=1e-3,
-        noise=0.2,
+        lr_pi=1e-3,
+        noise=0.1,
         replay_start_size=5000,
-        replay_buffer_size=100000,
-        minibatch_size=64,
+        replay_buffer_size=1e6,
+        minibatch_size=100,
         discount_factor=0.99,
-        polyak_rate=0.001,
+        polyak_rate=0.005,
         update_frequency=1,
         final_frame=2e6, # Anneal LR and clip until here
-        hidden1=64,
-        hidden2=64,
+        hidden1=400,
+        hidden2=300,
         device=torch.device('cuda')
 ):
     final_anneal_step = (final_frame - replay_start_size) // update_frequency
 
     def _ddpg(env, writer=DummyWriter()):
         value_model = fc_value(env, hidden1, hidden2).to(device)
-        value_optimizer = Adam(value_model.parameters(), lr=lr_q, weight_decay=weight_decay)
+        value_optimizer = Adam(value_model.parameters(), lr=lr_q)
         q = QContinuous(
             value_model,
             value_optimizer,
