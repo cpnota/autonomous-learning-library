87f385ca7baa4b219bc1b21e18011ead2fe6e308
Chris Nota <cpnota@gmail.com>
2020-01-07 18:23:57 -0500

    clean up ddpg code

--- all/agents/ddpg.py
+++ all/agents/ddpg.py
@@ -1,6 +1,6 @@
 import torch
 from torch.distributions.normal import Normal
-from all.nn import weighted_mse_loss
+from torch.nn.functional import mse_loss
 from ._agent import Agent
 
 class DDPG(Agent):
@@ -24,26 +24,24 @@ class DDPG(Agent):
         self.update_frequency = update_frequency
         self.minibatch_size = minibatch_size
         self.discount_factor = discount_factor
+        # private
         self._noise = Normal(0, noise * torch.tensor((action_space.high - action_space.low) / 2).to(policy.device))
         self._low = torch.tensor(action_space.low, device=policy.device)
         self._high = torch.tensor(action_space.high, device=policy.device)
-        # data
-        self.env = None
-        self.state = None
-        self.action = None
-        self.frames_seen = 0
+        self._state = None
+        self._action = None
+        self._frames_seen = 0
 
     def act(self, state, reward):
-        self._store_transition(state, reward)
+        self._store_transition(self._state, self._action, reward, state)
         self._train()
-        self.state = state
-        self.action = self._choose_action(state)
-        return self.action
+        self._state = state
+        self._action = self._choose_action(state)
+        return self._action
 
-    def _store_transition(self, state, reward):
-        if self.state and not self.state.done:
-            self.frames_seen += 1
-            self.replay_buffer.store(self.state, self.action, reward, state)
+    def _store_transition(self, state, action, reward, next_state):
+        if state and not state.done:
+            self.replay_buffer.store(state, action, reward, next_state)
 
     def _choose_action(self, state):
         action = self.policy.eval(state)
@@ -54,13 +52,13 @@ class DDPG(Agent):
 
     def _train(self):
         if self._should_train():
-            (states, actions, rewards, next_states, weights) = self.replay_buffer.sample(
-                self.minibatch_size)
+            # sample transitions from buffer
+            (states, actions, rewards, next_states, _) = self.replay_buffer.sample(self.minibatch_size)
 
             # train q-network
             q_values = self.q(states, torch.cat(actions))
             targets = rewards + self.discount_factor * self.q.target(next_states, self.policy.target(next_states))
-            loss = weighted_mse_loss(q_values, targets, weights)
+            loss = mse_loss(q_values, targets)
             self.q.reinforce(loss)
 
             # train policy
@@ -69,10 +67,6 @@ class DDPG(Agent):
             self.policy.reinforce(loss)
             self.q.zero_grad()
 
-            # update replay buffer
-            td_errors = targets - q_values
-            self.replay_buffer.update_priorities(td_errors.abs())
-
     def _should_train(self):
-        return (self.frames_seen > self.replay_start_size and
-                self.frames_seen % self.update_frequency == 0)
+        self._frames_seen += 1
+        return self._frames_seen > self.replay_start_size and self._frames_seen % self.update_frequency == 0
