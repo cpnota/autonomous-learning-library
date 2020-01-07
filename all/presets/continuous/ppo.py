918708c31c3eccf1a78394da64a8347f730f36c3
Chris Nota <cpnota@gmail.com>
2020-01-07 16:30:39 -0500

    tune ppo

--- all/presets/continuous/ppo.py
+++ all/presets/continuous/ppo.py
@@ -2,9 +2,10 @@
 import torch
 from torch.optim import Adam
 from torch.optim.lr_scheduler import CosineAnnealingLR
-from all.agents import PPO
 from all import nn
+from all.agents import PPO
 from all.approximation import VNetwork, FeatureNetwork
+from all.bodies import TimeFeature
 from all.logging import DummyWriter
 from all.optim import LinearScheduler
 from all.policies import GaussianPolicy
@@ -12,36 +13,36 @@ from all.policies import GaussianPolicy
 def fc_features(env):
     return nn.Sequential(
         nn.Flatten(),
-        nn.Linear(env.state_space.shape[0], 64),
+        nn.Linear(env.state_space.shape[0] + 1, 400),
         nn.ReLU(),
     )
 
 def fc_v():
     return nn.Sequential(
-        nn.Linear(64, 64),
+        nn.Linear(400, 300),
         nn.ReLU(),
-        nn.Linear(64, 1)
+        nn.Linear(300, 1)
     )
 
 def fc_policy(env):
     return nn.Sequential(
-        nn.Linear(64, 64),
+        nn.Linear(400, 300),
         nn.ReLU(),
-        nn.Linear(64, env.action_space.shape[0] * 2)
+        nn.Linear(300, env.action_space.shape[0] * 2)
     )
 
 def ppo(
         clip_grad=0.5,
-        discount_factor=0.99,
-        lam=0.95,  # GAE lambda (similar to e-traces)
+        discount_factor=0.98,
+        lam=0.95,  # GAE lambda
         lr=3e-4,  # Adam learning rate
         eps=1e-5,  # Adam stability
         entropy_loss_scaling=0.01,
         value_loss_scaling=0.5,
-        clip_initial=0.1,
+        clip_initial=0.2,
         clip_final=0.01,
         final_frame=2e6, # Anneal LR and clip until here
-        epochs=10,
+        epochs=20,
         minibatches=4,
         n_envs=32,
         n_steps=128,
@@ -97,7 +98,7 @@ def ppo(
             ),
         )
 
-        return PPO(
+        return TimeFeature(PPO(
             features,
             v,
             policy,
@@ -117,7 +118,7 @@ def ppo(
             lam=lam,
             entropy_loss_scaling=entropy_loss_scaling,
             writer=writer,
-        )
+        ))
 
     return _ppo, n_envs
 
