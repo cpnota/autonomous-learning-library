d10f4207cb370f90b4c36accde8729892c273724
Chris Nota <cpnota@gmail.com>
2020-01-07 19:02:16 -0500

    clean up ppo

--- all/agents/ppo.py
+++ all/agents/ppo.py
@@ -11,64 +11,71 @@ class PPO(Agent):
             features,
             v,
             policy,
-            epsilon=0.2,
+            discount_factor=0.99,
+            entropy_loss_scaling=0.01,
             epochs=4,
+            epsilon=0.2,
+            lam=0.95,
             minibatches=4,
             n_envs=None,
             n_steps=4,
-            discount_factor=0.99,
-            lam=0.95,
-            entropy_loss_scaling=0.01,
             writer=DummyWriter()
     ):
         if n_envs is None:
             raise RuntimeError("Must specify n_envs.")
+        # objects
         self.features = features
         self.v = v
         self.policy = policy
+        self.writer = writer
+        # hyperparameters
+        self.discount_factor = discount_factor
+        self.entropy_loss_scaling = entropy_loss_scaling
+        self.epochs = epochs
+        self.epsilon = epsilon
+        self.lam = lam
+        self.minibatches = minibatches
         self.n_envs = n_envs
         self.n_steps = n_steps
-        self.discount_factor = discount_factor
-        self.lam = lam
-        self.entropy_loss_scaling = entropy_loss_scaling
-        self.writer = writer
+        # private
         self._states = None
         self._actions = None
-        self._epsilon = epsilon
-        self._epochs = epochs
         self._batch_size = n_envs * n_steps
-        self._minibatches = minibatches
         self._buffer = self._make_buffer()
-        self._features = []
 
     def act(self, states, rewards):
-        self._store_transitions(rewards)
+        self._store_transitions(self._states, self._actions, rewards)
         self._train(states)
         self._states = states
         self._actions = self.policy.eval(self.features.eval(states)).sample()
         return self._actions
 
-    def _store_transitions(self, rewards):
-        if self._states:
-            self._buffer.store(self._states, self._actions, rewards)
+    def _store_transitions(self, states, actions, rewards):
+        if states:
+            self._buffer.store(states, actions, rewards)
 
-    def _train(self, _states):
+    def _train(self, next_states):
         if len(self._buffer) >= self._batch_size:
-            states, actions, advantages = self._buffer.advantages(_states)
-            with torch.no_grad():
-                features = self.features.eval(states)
-                pi_0 = self.policy.eval(features).log_prob(actions)
-                targets = self.v.eval(features) + advantages
-            for _ in range(self._epochs):
-                self._train_epoch(states, actions, pi_0, advantages, targets)
+            # load trajectories from buffer
+            states, actions, advantages = self._buffer.advantages(next_states)
+            # compute target values
+            features = self.features.eval(states)
+            pi_0 = self.policy.eval(features).log_prob(actions)
+            targets = self.v.eval(features) + advantages
+            # train for several epochs
+            for _ in range(self.epochs):
+                self._train_epoch(states, actions, advantages, targets, pi_0)
 
-    def _train_epoch(self, states, actions, pi_0, advantages, targets):
-        minibatch_size = int(self._batch_size / self._minibatches)
+    def _train_epoch(self, states, actions, advantages, targets, pi_0):
+        # randomly permute the indexes to generate minibatches
+        minibatch_size = int(self._batch_size / self.minibatches)
         indexes = torch.randperm(self._batch_size)
-        for n in range(self._minibatches):
+        for n in range(self.minibatches):
+            # load the indexes for the minibatch
             first = n * minibatch_size
             last = first + minibatch_size
             i = indexes[first:last]
+            # perform a single training step
             self._train_minibatch(states[i], actions[i], pi_0[i], advantages[i], targets[i])
 
     def _train_minibatch(self, states, actions, pi_0, advantages, targets):
@@ -79,7 +86,7 @@ class PPO(Agent):
         pi_i = distribution.log_prob(actions)
         # compute losses
         value_loss = mse_loss(values, targets)
-        policy_gradient_loss = self._policy_loss(pi_i, pi_0, advantages)
+        policy_gradient_loss = self._clipped_policy_gradient_loss(pi_0, pi_i, advantages)
         entropy_loss = -distribution.entropy().mean()
         policy_loss = policy_gradient_loss + self.entropy_loss_scaling * entropy_loss
         # backward pass
@@ -90,10 +97,10 @@ class PPO(Agent):
         self.writer.add_loss('policy_gradient', policy_gradient_loss.detach())
         self.writer.add_loss('entropy', -entropy_loss.detach())
 
-    def _policy_loss(self, pi_i, pi_0, advantages):
+    def _clipped_policy_gradient_loss(self, pi_0, pi_i, advantages):
         ratios = torch.exp(pi_i - pi_0)
         surr1 = ratios * advantages
-        epsilon = self._epsilon
+        epsilon = self.epsilon
         surr2 = torch.clamp(ratios, 1.0 - epsilon, 1.0 + epsilon) * advantages
         return -torch.min(surr1, surr2).mean()
 
