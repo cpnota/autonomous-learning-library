34c95e00837117ad38f9f9bb5699ef1efec5bb2c
Chris Nota <cpnota@gmail.com>
2020-06-24 17:38:09 -0400

    add environment tests

---
+++ all/environments/gym_test.py
@@ -0,0 +1,34 @@
+import unittest
+from all.environments import GymEnvironment
+
+
+class GymEnvironmentTest(unittest.TestCase):
+    def test_reset(self):
+        env = GymEnvironment('CartPole-v0')
+        state = env.reset()
+        self.assertEqual(state.observation.shape, (1, 4))
+        self.assertEqual(state.reward, 0)
+        self.assertFalse(state.done)
+        self.assertEqual(state.mask, 1)
+
+    def test_step(self):
+        env = GymEnvironment('CartPole-v0')
+        env.reset()
+        state = env.step(1)
+        self.assertEqual(state.observation.shape, (1, 4))
+        self.assertEqual(state.reward, 1.)
+        self.assertFalse(state.done)
+        self.assertEqual(state.mask, 1)
+
+
+    def test_step_until_done(self):
+        env = GymEnvironment('CartPole-v0')
+        env.reset()
+        for _ in range(100):
+            state = env.step(1)
+            if state.done:
+                break
+        self.assertEqual(state.observation.shape, (1, 4))
+        self.assertEqual(state.reward, 1.)
+        self.assertTrue(state.done)
+        self.assertEqual(state.mask, 0)
