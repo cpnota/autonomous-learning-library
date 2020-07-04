33aa04dba5b1f19b43d06108e4e2c22c80e6b652
Chris Nota <cpnota@gmail.com>
2020-07-04 14:25:07 -0400

    fix gaussian_test

--- all/policies/gaussian_test.py
+++ all/policies/gaussian_test.py
@@ -59,10 +59,10 @@ class TestGaussian(unittest.TestCase):
     def test_eval(self):
         state = State(torch.randn(1, STATE_DIM))
         dist = self.policy.no_grad(state)
-        tt.assert_almost_equal(dist.mean, torch.tensor([[-0.229, 0.43, -0.058]]), decimal=3)
+        tt.assert_almost_equal(dist.mean, torch.tensor([[-0.233, 0.459, -0.058]]), decimal=3)
         tt.assert_almost_equal(dist.entropy(), torch.tensor([4.251]), decimal=3)
         best = self.policy.eval(state)
-        tt.assert_almost_equal(best, torch.tensor([[-0.229, 0.43, -0.058]]), decimal=3)
+        tt.assert_almost_equal(best, torch.tensor([[-0.233, 0.459, -0.058]]), decimal=3)
 
 
 if __name__ == '__main__':
