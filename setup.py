d3cf5aa181408e3f6b8fada631babf56b26e2ba7
Chris Nota <cpnota@gmail.com>
2020-11-03 11:47:48 -0500

    another try

--- setup.py
+++ setup.py
@@ -1,6 +1,29 @@
 from setuptools import setup, find_packages
 
 
+def union(*deps):
+    return list(set().union(*[extras[dep] for dep in deps]))
+
+extras = {
+    "envs": [
+        "gym[atari,box2d]>=0.17.2",  # common environments
+        "pybullet>=3.0.6",           # continuous environments
+    ],
+    "test": [
+        "pylint>=2.6.0",             # code quality tool
+        "torch-testing>=0.0.2",      # pytorch assertion library
+    ],
+    "docs": [
+        "sphinx>=3.2.1",              # documentation library
+        "sphinx-autobuild>=2020.9.1", # documentation live reload
+        "sphinx-rtd-theme>=0.5.0",    # documentation theme
+        "sphinx-automodapi>=0.13",    # autogenerate docs for modules
+    ]
+}
+
+extras["dev"] = union("envs", "test", "docs")
+
+
 setup(
     name="autonomous-learning-library",
     version="0.6.2",
@@ -23,26 +46,13 @@ setup(
         ],
     },
     install_requires=[
-        "gym>=0.17.2",                    # common environment interface
-        "numpy>=1.18.0",                  # math library
-        "matplotlib>=3.3.0",              # plotting library
-        "opencv-python>=3.,<4.",          # used by atari wrappers
-        "torch>=1.5.1",                   # core deep learning library
-        "tensorboard>=2.3.0",             # logging and visualization
-        "tensorboardX>=2.1.0",            # tensorboard/pytorch compatibility
+        "gym>=0.17.2",             # common environment interface
+        "numpy>=1.18.0",           # math library
+        "matplotlib>=3.3.0",       # plotting library
+        "opencv-python>=3.,<4.",   # used by atari wrappers
+        "torch>=1.5.1",            # core deep learning library
+        "tensorboard>=2.3.0",      # logging and visualization
+        "tensorboardX>=2.1.0",     # tensorboard/pytorch compatibility
     ],
-    extras_require={
-        "envs": [
-            "gym[atari,box2d]>=0.17.2",   # common environments
-            "pybullet>=3.0.6",            # continuous environments
-        ],
-        "dev": [
-            "pylint>=2.6.0",              # code quality tool
-            "torch-testing>=0.0.2",       # pytorch assertion library
-            "sphinx>=3.2.1",              # documentation library
-            "sphinx-autobuild>=2020.9.1", # documentation live reload
-            "sphinx-rtd-theme>=0.5.0",    # documentation theme
-            "sphinx-automodapi>=0.13",    # autogenerate docs for modules
-        ]
-    },
+    extras_require=extras
 )
