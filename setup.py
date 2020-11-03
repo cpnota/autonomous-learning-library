1f3b074eac879d847f8014b686679018ec048625
Chris Nota <cpnota@gmail.com>
2020-11-03 11:18:44 -0500

    move torch to core

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 from setuptools import setup, find_packages
 
+
 extras = {
+    "envs": [
+        "gym[atari,box2d]>=0.17.2",  # common environments
+        "pybullet>=3.0.6"            # continuous environments
+    ],
+    "test": [
+        "pylint>=2.6.0",             # code quality tool
+        "torch-testing>=0.0.2",      # pytorch assertion library
+    ],
     "docs": [
         "sphinx>=3.2.1",
         "sphinx-autobuild>=2020.9.1",
         "sphinx-rtd-theme>=0.5.0",
-        "sphinx-automodapi>=0.13"
+        "sphinx-automodapi>=0.13",
     ],
-    "dev": [
-        "pylint>=2.6.0",             # code quality tool
-        "torch-testing>=0.0.2",      # pytorch assertion library
-    ],
-    "envs": [
-        "gym[atari,box2d]>=0.17.2",  # common environments
-        "pybullet>=3.0.6"            # continuous environments
-    ]
 }
 
-extras["all"] = list(set().union(*extras.values()))
+
+def union(*deps):
+    return list(set().union(*[extras[dep] for dep in deps]))
+
+extras["dev"] = union("envs", "docs", "test")
 
 setup(
     name="autonomous-learning-library",
@@ -45,10 +50,14 @@ setup(
         "numpy>=1.18.0",           # math library
         "matplotlib>=3.3.0",       # plotting library
         "opencv-python>=3.,<4.",   # used by atari wrappers
-        "torch>=1.5.1",            # deep learning
-        "torchvision>=0.6.1",      # additional utilities
+        "torch>=1.5.1",            # core deep learning library
         "tensorboard>=2.3.0",      # logging and visualization
-        "tensorboardX>=2.3.0",     # tensorboard/pytorch compatibility
+        "tensorboardX>=2.1.0",     # tensorboard/pytorch compatibility
     ],
-    extras_require=extras,
+    extras_require={
+        "envs": [
+            "gym[atari,box2d]>=0.17.2",  # common environments
+            "pybullet>=3.0.6"            # continuous environments
+        ],
+    },
 )
