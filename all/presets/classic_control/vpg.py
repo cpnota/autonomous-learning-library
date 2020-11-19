dcfe6ad753885c93d27466d3254c92b5d7ec05da
Chris Nota <cpnota@gmail.com>
2020-11-19 12:18:13 -0500

    add vpg cc preset

--- all/presets/classic_control/vpg.py
+++ all/presets/classic_control/vpg.py
@@ -1,64 +1,97 @@
+import copy
 from torch.optim import Adam
-from all.agents import VPG
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from all.agents import VPG, VPGTestAgent
 from all.approximation import VNetwork, FeatureNetwork
+from all.bodies import DeepmindAtariBody
 from all.logging import DummyWriter
 from all.policies import SoftmaxPolicy
 from .models import fc_relu_features, fc_policy_head, fc_value_head
+from ..builder import preset_builder
+from ..preset import Preset
 
 
-def vpg(
-        # Common settings
-        device="cpu",
-        discount_factor=0.99,
-        # Adam optimizer settings
-        lr=5e-3,
-        # Batch settings
-        min_batch_size=500,
-        # Model construction
-        feature_model_constructor=fc_relu_features,
-        value_model_constructor=fc_value_head,
-        policy_model_constructor=fc_policy_head
-):
+default_hyperparameters = {
+    # Common settings
+    "discount_factor": 0.99,
+    # Adam optimizer settings
+    "lr_v": 5e-3,
+    "lr_pi": 1e-4,
+    "eps": 1.5e-4,
+    # Other optimization settings
+    "clip_grad": 0.5,
+    "value_loss_scaling": 0.25,
+    "min_batch_size": 500,
+    # Model construction
+    "feature_model_constructor": fc_relu_features,
+    "value_model_constructor": fc_value_head,
+    "policy_model_constructor": fc_policy_head
+}
+
+class VPGClassicControlPreset(Preset):
     """
-    Vanilla Policy Gradient classic control preset.
+    Vanilla Policy Gradient (VPG) Classic Control preset.
 
     Args:
-        device (str): The device to load parameters and buffers onto for this agent.
+        env (all.environments.GymEnvironment): The environment for which to construct the agent.
+        device (torch.device, optional): The device on which to load the agent.
+
+    Keyword Args:
         discount_factor (float): Discount factor for future rewards.
-        last_frame (int): Number of frames to train.
         lr (float): Learning rate for the Adam optimizer.
+        eps (float): Stability parameters for the Adam optimizer.
+        clip_grad (float): The maximum magnitude of the gradient for any given parameter.
+            Set to 0 to disable.
+        value_loss_scaling (float): Coefficient for the value function loss.
         min_batch_size (int): Continue running complete episodes until at least this many
             states have been seen since the last update.
         feature_model_constructor (function): The function used to construct the neural feature model.
         value_model_constructor (function): The function used to construct the neural value model.
         policy_model_constructor (function): The function used to construct the neural policy model.
     """
-    def _vpg(env, writer=DummyWriter()):
-        feature_model = feature_model_constructor(env).to(device)
-        value_model = value_model_constructor().to(device)
-        policy_model = policy_model_constructor(env).to(device)
+    def __init__(self, env, device="cuda", **hyperparameters):
+        hyperparameters = {**default_hyperparameters, **hyperparameters}
+        super().__init__()
+        self.value_model = hyperparameters['value_model_constructor']().to(device)
+        self.policy_model = hyperparameters['policy_model_constructor'](env).to(device)
+        self.feature_model = hyperparameters['feature_model_constructor'](env).to(device)
+        self.hyperparameters = hyperparameters
+        self.device = device
 
-        feature_optimizer = Adam(feature_model.parameters(), lr=lr)
-        value_optimizer = Adam(value_model.parameters(), lr=lr)
-        policy_optimizer = Adam(policy_model.parameters(), lr=lr)
+    def agent(self, writer=DummyWriter(), train_steps=float('inf')):
+        n_updates = train_steps / self.hyperparameters["min_batch_size"]
+
+        feature_optimizer = Adam(self.feature_model.parameters(), lr=self.hyperparameters["lr_pi"], eps=self.hyperparameters["eps"])
+        value_optimizer = Adam(self.value_model.parameters(), lr=self.hyperparameters["lr_v"], eps=self.hyperparameters["eps"])
+        policy_optimizer = Adam(self.policy_model.parameters(), lr=self.hyperparameters["lr_pi"], eps=self.hyperparameters["eps"])
 
         features = FeatureNetwork(
-            feature_model,
+            self.feature_model,
             feature_optimizer,
+            clip_grad=self.hyperparameters["clip_grad"],
             writer=writer
         )
+
         v = VNetwork(
-            value_model,
+            self.value_model,
             value_optimizer,
+            loss_scaling=self.hyperparameters["value_loss_scaling"],
+            clip_grad=self.hyperparameters["clip_grad"],
             writer=writer
         )
+
         policy = SoftmaxPolicy(
-            policy_model,
+            self.policy_model,
             policy_optimizer,
+            clip_grad=self.hyperparameters["clip_grad"],
             writer=writer
         )
-        return VPG(features, v, policy, discount_factor=discount_factor, min_batch_size=min_batch_size)
-    return _vpg
 
+        return VPG(features, v, policy, discount_factor=self.hyperparameters["discount_factor"], min_batch_size=self.hyperparameters["min_batch_size"])
 
-__all__ = ["vpg"]
+    def test_agent(self):
+        features = FeatureNetwork(copy.deepcopy(self.feature_model))
+        policy = SoftmaxPolicy(copy.deepcopy(self.policy_model))
+        return VPGTestAgent(features, policy)
+
+vpg = preset_builder('vpg', default_hyperparameters, VPGClassicControlPreset)
