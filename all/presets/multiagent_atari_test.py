fefdb64a7999eed79032fd3d3446a11c1d91d652
Chris Nota <cpnota@gmail.com>
2021-01-11 12:25:41 -0500

    add multiagent atari preset unittest

---
+++ all/presets/multiagent_atari_test.py
@@ -0,0 +1,42 @@
+import os
+import unittest
+import torch
+from all.environments import MultiagentAtariEnv
+from all.logging import DummyWriter
+from all.presets.atari import dqn
+from all.presets.multiagent_atari import IndependentMultiagentAtariPreset
+
+
+class TestAtariPresets(unittest.TestCase):
+    def setUp(self):
+        self.env = MultiagentAtariEnv('pong_v1', device='cpu')
+        self.env.reset()
+
+    def tearDown(self):
+        if os.path.exists('test_preset.pt'):
+            os.remove('test_preset.pt')
+
+    def test_independent(self):
+        env = MultiagentAtariEnv('pong_v1', device='cpu')
+        presets = {
+            agent_id : dqn().device('cpu').env(env.subenvs[agent_id]).build()
+            for agent_id in env.agents
+        }
+        self.validate_preset(IndependentMultiagentAtariPreset(presets), env)
+
+    def validate_preset(self, preset, env):
+        # normal agent
+        agent = preset.agent(writer=DummyWriter(), train_steps=100000)
+        agent.act(self.env.last())
+        # test agent
+        test_agent = preset.test_agent()
+        test_agent.act(self.env.last())
+        # test save/load
+        preset.save('test_preset.pt')
+        preset = torch.load('test_preset.pt')
+        test_agent = preset.test_agent()
+        test_agent.act(self.env.last())
+
+
+if __name__ == "__main__":
+    unittest.main()
