9df9cdd0aa6b9de9c60fab65990bfd6130fb6dda
Chris Nota <cpnota@gmail.com>
2021-01-11 11:35:54 -0500

    add integration tests

---
+++ integration/multiagent_atari_test.py
@@ -0,0 +1,40 @@
+import unittest
+import torch
+from all.environments import MultiagentAtariEnv
+from all.presets.multiagent_atari import IndependentMultiagentAtariPreset
+from all.presets.atari import dqn
+from validate_agent import validate_multiagent
+
+
+CPU = torch.device("cpu")
+if torch.cuda.is_available():
+    CUDA = torch.device("cuda")
+else:
+    print(
+        "WARNING: CUDA is not available!",
+        "Running presets in cpu mode.",
+        "Enable CUDA for full test coverage!",
+    )
+    CUDA = torch.device("cpu")
+
+
+class TestMultiagentAtariPresets(unittest.TestCase):
+    def test_independent(self):
+        env = MultiagentAtariEnv('pong_v1', device=CPU)
+        presets = {
+            agent_id : dqn().device(CPU).env(env.subenvs[agent_id]).build()
+            for agent_id in env.agents
+        }
+        validate_multiagent(IndependentMultiagentAtariPreset(presets), env)
+
+    def test_independent_cuda(self):
+        env = MultiagentAtariEnv('pong_v1', device=CUDA)
+        presets = {
+            agent_id : dqn().device(CUDA).env(env.subenvs[agent_id]).build()
+            for agent_id in env.agents
+        }
+        validate_multiagent(IndependentMultiagentAtariPreset(presets), env)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- integration/validate_agent.py
+++ integration/validate_agent.py
@@ -1,6 +1,6 @@
 import os
 from all.logging import DummyWriter
-from all.experiments import SingleEnvExperiment, ParallelEnvExperiment
+from all.experiments import SingleEnvExperiment, ParallelEnvExperiment, MultiagentEnvExperiment
 
 
 class TestSingleEnvExperiment(SingleEnvExperiment):
@@ -14,6 +14,11 @@ class TestParallelEnvExperiment(ParallelEnvExperiment):
         os.makedirs(logdir, exist_ok=True)
         return DummyWriter()
 
+class TestMultiagentEnvExperiment(MultiagentEnvExperiment):
+    def _make_writer(self, logdir, agent_name, env_name, write_loss):
+        os.makedirs(logdir, exist_ok=True)
+        return DummyWriter()
+
 
 def validate_agent(agent, env):
     preset = agent.env(env).build()
@@ -21,5 +26,10 @@ def validate_agent(agent, env):
         experiment = TestParallelEnvExperiment(preset, env, quiet=True)
     else:
         experiment = TestSingleEnvExperiment(preset, env, quiet=True)
+    experiment.train(episodes=2)
+    experiment.test(episodes=2)
+
+def validate_multiagent(preset, env):
+    experiment = TestMultiagentEnvExperiment(preset, env, quiet=True)
     experiment.train(episodes=2)
     experiment.test(episodes=2)
