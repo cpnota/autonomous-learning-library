9e8b27c78be9c1c7df58f06170f70b09af891205
Chris Nota <cpnota@gmail.com>
2021-01-12 13:05:32 -0500

    update ppo continuous preset to use identity features

--- all/presets/continuous/models/__init__.py
+++ all/presets/continuous/models/__init__.py
@@ -47,23 +47,11 @@ def fc_soft_policy(env, hidden1=400, hidden2=300):
         nn.Linear0(hidden2, env.action_space.shape[0] * 2),
     )
 
-
-def fc_actor_critic(env, hidden1=400, hidden2=300):
-    features = nn.Sequential(
+def fc_policy(env, hidden1=400, hidden2=300):
+    return nn.Sequential(
         nn.Linear(env.state_space.shape[0] + 1, hidden1),
         nn.ReLU(),
-    )
-
-    v = nn.Sequential(
-        nn.Linear(hidden1, hidden2),
-        nn.ReLU(),
-        nn.Linear(hidden2, 1)
-    )
-
-    policy = nn.Sequential(
         nn.Linear(hidden1, hidden2),
         nn.ReLU(),
         nn.Linear(hidden2, env.action_space.shape[0] * 2)
     )
-
-    return features, v, policy
