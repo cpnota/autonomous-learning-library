2b6bda0fbad9d20ca1aabe994a3c09d31601356d
Chris Nota <cpnota@gmail.com>
2021-01-13 15:32:10 -0500

    make log variance independent of state

--- all/presets/continuous/models/__init__.py
+++ all/presets/continuous/models/__init__.py
@@ -5,6 +5,7 @@ All models assume that a feature representing the
 current timestep is used in addition to the features
 received from the environment.
 '''
+import torch
 from all import nn
 
 
@@ -48,11 +49,19 @@ def fc_soft_policy(env, hidden1=400, hidden2=300):
     )
 
 
-def fc_policy(env, hidden1=400, hidden2=300):
-    return nn.Sequential(
-        nn.Linear(env.state_space.shape[0] + 1, hidden1),
-        nn.ReLU(),
-        nn.Linear(hidden1, hidden2),
-        nn.ReLU(),
-        nn.Linear(hidden2, env.action_space.shape[0] * 2)
-    )
+class fc_policy(nn.Module):
+    def __init__(self, env, hidden1=400, hidden2=300):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(env.state_space.shape[0] + 1, hidden1),
+            nn.Tanh(),
+            nn.Linear(hidden1, hidden2),
+            nn.Tanh(),
+            nn.Linear(hidden2, env.action_space.shape[0])
+        )
+        self.log_stds = nn.Parameter(torch.zeros(env.action_space.shape[0]))
+
+    def forward(self, x):
+        means = self.model(x)
+        stds = self.log_stds.expand(*means.shape)
+        return torch.cat((means, stds), 1)
