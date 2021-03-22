60328c114a73a43908124771b0df36cd84aca17c
Chris Nota <cpnota@gmail.com>
2021-03-22 17:39:22 -0400

    refactor the way pybullet imports work

--- all/environments/pybullet_test.py
+++ all/environments/pybullet_test.py
@@ -13,7 +13,7 @@ class PybulletEnvironmentTest(unittest.TestCase):
         self.assertEqual(env.name, 'HalfCheetahBulletEnv-v0')
 
     def test_reset(self):
-        env = PybulletEnvironment('HalfCheetahBulletEnv-v0')
+        env = PybulletEnvironment('cheetah')
         state = env.reset()
         self.assertEqual(state.observation.shape, (26,))
         self.assertEqual(state.reward, 0.)
@@ -21,7 +21,7 @@ class PybulletEnvironmentTest(unittest.TestCase):
         self.assertEqual(state.mask, 1)
 
     def test_step(self):
-        env = PybulletEnvironment('HalfCheetahBulletEnv-v0')
+        env = PybulletEnvironment('cheetah')
         env.seed(0)
         state = env.reset()
         state = env.step(env.action_space.sample())
