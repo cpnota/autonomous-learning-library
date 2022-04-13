5d83612ffe1da146e8015948e0b4419d9722454c
Nota, Christopher <cnota@irobot.com>
2022-04-13 12:29:26 -0400

    update FeatureNetwork docstring

--- all/approximation/feature_network.py
+++ all/approximation/feature_network.py
@@ -4,15 +4,9 @@ from .approximation import Approximation
 
 class FeatureNetwork(Approximation):
     '''
-    A special type of Approximation that accumulates gradients before backpropagating them.
-    This is useful when features are shared between network heads.
-
-    The __call__ function caches the computation graph and detaches the output.
-    Then, various functions approximators may backpropagate to the output.
-    The reinforce() function will then backpropagate the accumulated gradients on the output
-    through the original computation graph.
+    An Approximation that accepts a state updates the observation key
+    based on the given model.
     '''
-
     def __init__(self, model, optimizer=None, name='feature', **kwargs):
         model = FeatureModule(model)
         super().__init__(model, optimizer, name=name, **kwargs)
