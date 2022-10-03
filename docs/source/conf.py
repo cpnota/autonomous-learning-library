43736f07c811140a7b641ac5f41d92344053f188
Nota, Christopher <cpnota@gmail.com>
2022-10-03 10:04:33 -0400

    version 0.8.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Chris Nota'
 author = 'Chris Nota'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.1'
+release = '0.8.2'
 
 # -- General configuration ---------------------------------------------------
 
