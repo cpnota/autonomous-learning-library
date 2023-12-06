f65cde620dfa9fd080039811063611367a2101a3
Nota, Christopher <cpnota@gmail.com>
2023-12-06 09:06:09 -0500

    update parallel env experiment test

--- all/experiments/parallel_env_experiment_test.py
+++ all/experiments/parallel_env_experiment_test.py
@@ -9,7 +9,7 @@ from all.experiments.single_env_experiment_test import MockLogger
 
 class MockExperiment(ParallelEnvExperiment):
     def _make_logger(self, logdir, agent_name, env_name, verbose, logger):
-        self._logger = MockLogger(self, agent_name + '_' + env_name, verbose)
+        self._logger = MockLogger(self, agent_name + "_" + env_name, verbose)
         return self._logger
 
 
@@ -17,28 +17,28 @@ class TestParallelEnvExperiment(unittest.TestCase):
     def setUp(self):
         np.random.seed(0)
         torch.manual_seed(0)
-        self.env = GymEnvironment('CartPole-v0')
-        self.env.seed(0)
+        self.env = GymEnvironment("CartPole-v0")
+        self.env.reset(seed=0)
         self.experiment = MockExperiment(self.make_agent(), self.env, quiet=True)
-        self.experiment._env.seed(0)
+        self.experiment._env.reset(seed=0)
 
     def test_adds_default_label(self):
         self.assertEqual(self.experiment._logger.label, "a2c_CartPole-v0")
 
     def test_adds_custom_label(self):
-        env = GymEnvironment('CartPole-v0')
-        experiment = MockExperiment(self.make_agent(), env, name='a2c', quiet=True)
+        env = GymEnvironment("CartPole-v0")
+        experiment = MockExperiment(self.make_agent(), env, name="a2c", quiet=True)
         self.assertEqual(experiment._logger.label, "a2c_CartPole-v0")
 
     def test_writes_training_returns_eps(self):
-        self.experiment.train(episodes=3)
+        self.experiment.train(episodes=4)
         np.testing.assert_equal(
             self.experiment._logger.data["eval/returns/episode"]["steps"],
-            np.array([1, 2, 3]),
+            np.array([1, 2, 3, 3]),
         )
         np.testing.assert_equal(
             self.experiment._logger.data["eval/returns/episode"]["values"],
-            np.array([10., 12., 19.]),
+            np.array([12.0, 13.0, 16.0, 16.0]),
         )
 
     def test_writes_test_returns(self):
@@ -55,13 +55,17 @@ class TestParallelEnvExperiment(unittest.TestCase):
         )
 
     def test_writes_loss(self):
-        experiment = MockExperiment(self.make_agent(), self.env, quiet=True, verbose=True)
+        experiment = MockExperiment(
+            self.make_agent(), self.env, quiet=True, verbose=True
+        )
         self.assertTrue(experiment._logger.verbose)
-        experiment = MockExperiment(self.make_agent(), self.env, quiet=True, verbose=False)
+        experiment = MockExperiment(
+            self.make_agent(), self.env, quiet=True, verbose=False
+        )
         self.assertFalse(experiment._logger.verbose)
 
     def make_agent(self):
-        return a2c.device('cpu').env(self.env).build()
+        return a2c.device("cpu").env(self.env).build()
 
 
 if __name__ == "__main__":
