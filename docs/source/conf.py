ea80322a4e814609c12d2675d3c263f60e00842e
Nota, Christopher <cpnota@gmail.com>
2023-12-07 17:04:22 -0500

    update docs version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Chris Nota'
 author = 'Chris Nota'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.2'
+release = '0.9.0'
 
 # -- General configuration ---------------------------------------------------
 
