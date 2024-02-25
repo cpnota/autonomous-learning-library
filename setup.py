4e01e46b64e7b749642ec5590ce626dc1a9edd1c
Nota, Christopher <cpnota@gmail.com>
2024-02-25 12:02:31 -0500

    update publish workflow and add alpha tag

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ extras["dev"] = extras["all"] + extras["test"] + extras["docs"]
 
 setup(
     name="autonomous-learning-library",
-    version="0.9.0",
+    version="0.9.1-alpha.1",
     description=("A library for building reinforcement learning agents in Pytorch"),
     packages=find_packages(),
     url="https://github.com/cpnota/autonomous-learning-library.git",
