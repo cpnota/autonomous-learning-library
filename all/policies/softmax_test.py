fix softmax test problem

all/policies/softmax_test.py
@@ -23,7 +23,7 @@ class TestSoftmax(unittest.TestCase):
         dist1 = self.policy(state1)
         action1 = dist1.sample()
         log_prob1 = dist1.log_prob(action1)
-        self.assertEqual(action1.item(), 0)
+        self.assertEqual(action1.item(), 2)
 
         state2 = State(torch.randn(1, STATE_DIM))
         dist2 = self.policy(state2)
@@ -37,12 +37,12 @@ class TestSoftmax(unittest.TestCase):
         state3 = State(torch.randn(1, STATE_DIM))
         dist3 = self.policy(state3)
         action3 = dist3.sample()
-        self.assertEqual(action3.item(), 2)
+        self.assertEqual(action3.item(), 0)
 
     def test_multi_action(self):
         states = State(torch.randn(3, STATE_DIM))
         actions = self.policy(states).sample()
-        tt.assert_equal(actions, torch.tensor([2, 2, 0]))
+        tt.assert_equal(actions, torch.tensor([2, 0, 0]))
 
     def test_list(self):
         torch.manual_seed(1)
@@ -50,7 +50,7 @@ class TestSoftmax(unittest.TestCase):
         dist = self.policy(states)
         actions = dist.sample()
         log_probs = dist.log_prob(actions)
-        tt.assert_equal(actions, torch.tensor([1, 2, 1]))
+        tt.assert_equal(actions, torch.tensor([0, 0, 2]))
         loss = -(torch.tensor([[1, 2, 3]]) * log_probs).mean()
         self.policy.reinforce(loss)
 
@@ -64,17 +64,17 @@ class TestSoftmax(unittest.TestCase):
         # notice the values increase with each successive reinforce
         log_probs = self.policy(states).log_prob(actions)
         tt.assert_almost_equal(
-            log_probs, torch.tensor([-0.84, -0.62, -0.757]), decimal=3
+            log_probs, torch.tensor([-0.84, -1.325, -0.757]), decimal=3
         )
         self.policy.reinforce(loss(log_probs))
         log_probs = self.policy(states).log_prob(actions)
         tt.assert_almost_equal(
-            log_probs, torch.tensor([-0.811, -0.561, -0.701]), decimal=3
+            log_probs, torch.tensor([-0.855, -1.278, -0.726]), decimal=3
         )
         self.policy.reinforce(loss(log_probs))
         log_probs = self.policy(states).log_prob(actions)
         tt.assert_almost_equal(
-            log_probs, torch.tensor([-0.785, -0.51, -0.651]), decimal=3
+            log_probs, torch.tensor([-0.871, -1.234, -0.698]), decimal=3
         )
 
     def test_eval(self):
@@ -88,7 +88,7 @@ class TestSoftmax(unittest.TestCase):
             decimal=3,
         )
         best = self.policy.eval(states).sample()
-        tt.assert_equal(best, torch.tensor([2, 2, 0]))
+        tt.assert_equal(best, torch.tensor([2, 0, 0]))
 
 
 if __name__ == "__main__":